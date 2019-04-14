cdce4d5451a85cf1dac1a9f8fc0758b22cbce449
xbeast7 <xbeast7@gmail.com>
2019-04-14 20:35:48 +0900

    Part2 Chapter3.1 컨트롤러의 구현
    Part2 기본적인 기능의 게시물 관리
    Chapter3 등록 구현 - 컨트롤러와 프레젠테이션 계층

---
+++ ex01/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,43 @@
+package org.zerock.controller;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@Inject
+	private BoardService service;
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public void registerGET(BoardVO board, Model model) throws Exception {
+		
+		logger.info("regist post ......");
+		
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registerPOST(BoardVO board, Model model) throws Exception {
+		
+		logger.info("regist post ......");
+		logger.info(board.toString());
+		
+		service.regist(board);
+		
+		model.addAttribute("result", "success");
+		
+		return "/board/success";
+		
+	}
+}
