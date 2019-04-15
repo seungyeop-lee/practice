cc36c28ce5e0a72dee9497b34007cfc4dad48593
xbeast7 <xbeast7@gmail.com>
2019-04-15 23:44:49 +0900

    Part2 Chapter6.2 수정 처리
    Part2 기본적인 기능의 게시물 관리
    Chapter6 삭제/수정 처리

--- ex01/src/main/java/org/zerock/controller/BoardController.java
+++ ex01/src/main/java/org/zerock/controller/BoardController.java
@@ -71,4 +71,23 @@ public class BoardController {
 		return "redirect:/board/listAll";
 		
 	}
+	
+	@RequestMapping(value = "/modify", method = RequestMethod.GET)
+	public void modifyGET(@RequestParam("bno") int bno, Model model) throws Exception {
+		
+		model.addAttribute(service.read(bno));
+		
+	}
+	
+	@RequestMapping(value = "/modify", method = RequestMethod.POST)
+	public String modifyPOST(BoardVO board, RedirectAttributes rttr) throws Exception {
+		
+		logger.info("mod post......");
+		
+		service.modify(board);
+		rttr.addFlashAttribute("msg", "SUCCESS");
+		
+		return "redirect:/board/listAll";
+		
+	}
 }
