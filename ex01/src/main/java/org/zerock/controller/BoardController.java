4f4ff469630baf0f3287f2389be1152b07547864
xbeast7 <xbeast7@gmail.com>
2019-04-15 23:04:28 +0900

    Part2 Chapter5.1 BoardController의 기능 추가와 뷰 처리
    Part2 기본적인 기능의 게시물 관리
    Chapter5 조회 구현

--- ex01/src/main/java/org/zerock/controller/BoardController.java
+++ ex01/src/main/java/org/zerock/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
@@ -48,6 +49,14 @@ public class BoardController {
 		
 		logger.info("show all list......");
 		model.addAttribute("list", service.listAll());
+		
+	}
+	
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public void read(@RequestParam("bno") int bno, Model model) throws Exception {
+		
+		//속성이름을 지정하지 않을경우, 클래스 이름을 소문자로 시작해서 사용
+		model.addAttribute(service.read(bno));
 		
 	}
 }
