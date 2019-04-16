147639040f113692b4c2cf94e778e9062c0972cc
xbeast7 <xbeast7@gmail.com>
2019-04-16 22:46:03 +0900

    Part2 Chapter8.2 페이징 처리 개발에 필요한 지식
    Part2 기본적인 기능의 게시물 관리
    Chapter8 페이징 처리 - 영속(persistence)계층, 비즈니스 계층

--- ex01/src/main/resources/testquery.sql
+++ ex01/src/main/resources/testquery.sql
@@ -18,3 +18,10 @@ where bno = 1;
 -- 게시물의 삭제에 사용하는 SQL
 delete from tbl_board
 where bno = 1;
+
+-- 자가 복사(self copy)SQL
+insert into tbl_board (title, content, writer)
+(select title, content, writer from tbl_board);
+
+-- 테이블의 데이터 수 가져오는 SQL
+select count(*) from tbl_board;
