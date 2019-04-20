c5150800187af5e061525fc6950a416213c5e1b4
xbeast7 <xbeast7@gmail.com>
2019-04-20 21:12:41 +0900

    Part2 Chapter10.4 MyBatis 동적 SQL
    Part2 기본적인 기능의 게시물 관리
    Chapter10 검색 처리와 동적 SQL

--- ex01/src/test/java/org/zerock/test/BoardDAOTest.java
+++ ex01/src/test/java/org/zerock/test/BoardDAOTest.java
@@ -14,6 +14,7 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 import org.zerock.domain.BoardVO;
 import org.zerock.domain.Criteria;
+import org.zerock.domain.SearchCriteria;
 import org.zerock.persistence.BoardDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -108,6 +109,27 @@ public class BoardDAOTest {
 		
 		logger.info("/board/read?bno=12&perPageNum=20");
 		logger.info(uriComponents.toString());
+		
+	}
+	
+	@Test
+	public void testDynamic1() throws Exception {
+		
+		SearchCriteria cri = new SearchCriteria();
+		cri.setPage(1);
+		cri.setKeyword("제");
+		cri.setSearchType("t");
+		
+		logger.info("==================");
+		
+		List<BoardVO> list = dao.listSearch(cri);
+		
+		for(BoardVO boardVO : list) {
+			logger.info(boardVO.getBno() + ": " + boardVO.getTitle());
+		}
+		
+		logger.info("==================");
+		logger.info("COUNT: " + dao.listSearchCount(cri));
 		
 	}
 }
