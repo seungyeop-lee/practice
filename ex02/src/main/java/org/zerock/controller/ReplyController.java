0e3146b6ffe130732a1bd4c20e9cc3833c66e7ee
xbeast7 <xbeast7@gmail.com>
2019-04-22 20:56:18 +0900

    Part3 Chapter3.2 특정 게시물의 전체 댓글 목록의 처리
    Part3 Ajax 댓글 처리
    Chapter3 REST방식의 ReplyController작성

--- ex02/src/main/java/org/zerock/controller/ReplyController.java
+++ ex02/src/main/java/org/zerock/controller/ReplyController.java
@@ -1,9 +1,12 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,6 +34,23 @@ public class ReplyController {
 		}
 		
 		return entity;
+	}
+	
+	@RequestMapping(value = "/all/{bno}", method = RequestMethod.GET)
+	public ResponseEntity<List<ReplyVO>> list(
+			@PathVariable("bno") Integer bno) {	//@PathVariable은 @RequestMapping경로에 {}에 해당하는 uri를 값으로 사용	 
+		
+		ResponseEntity<List<ReplyVO>> entity = null;
+		
+		try {
+			entity = new ResponseEntity<>(service.listReply(bno), HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		return entity;
+		
 	}
 	
 }
