832f1b06438ed8a497991fc3ec5cfbf1ffd5dace
xbeast7 <xbeast7@gmail.com>
2019-04-22 20:49:43 +0900

    Part3 Chapter3.1 등록 처리
    Part3 Ajax 댓글 처리
    Chapter3 REST방식의 ReplyController작성

---
+++ ex02/src/main/java/org/zerock/controller/ReplyController.java
@@ -0,0 +1,36 @@
+package org.zerock.controller;
+
+import javax.inject.Inject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.ReplyVO;
+import org.zerock.service.ReplyService;
+
+@RestController	//@Controller와 각 매소드에 @ResponseBody를 합친 어노테이션
+@RequestMapping("/replies")
+public class ReplyController {
+
+	@Inject
+	private ReplyService service;
+	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<String> register(@RequestBody ReplyVO vo) {	//@RequestBody는 Request를  데이터 자체로 인식
+		
+		ResponseEntity<String> entity = null;	//HTTP 상태 코드와 데이터를 같이 전송 할 때 사용
+		try {
+			service.addReply(vo);
+			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		} catch(Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		
+		return entity;
+	}
+	
+}
