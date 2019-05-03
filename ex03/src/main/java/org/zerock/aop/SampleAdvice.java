4ae066bf0ad561d4122afac13788e6e60bc19ca3
Seungyeop Lee <xbeast7@gmail.com>
2019-05-03 10:45:44 +0900

    Part4 Chapter3.3 실행 시에 전달되는 파라미터 파악하기
    Part4 AOP와 트랜잭션 처리
    Chapter3 AOP 연습하기

--- ex03/src/main/java/org/zerock/aop/SampleAdvice.java
+++ ex03/src/main/java/org/zerock/aop/SampleAdvice.java
@@ -1,5 +1,8 @@
 package org.zerock.aop;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -16,11 +19,22 @@ public class SampleAdvice {
 	//메소드 지정은 pointcut을 지정하는 문법으로 AspectJ언어의 문법
 	//org.zerock.service.MessageService로 시작하는 모든 클래스의 모든 메소드를 지정
 	@Before("execution(* org.zerock.service.MessageService*.*(..))")	
-	public void startLog() {
+	public void startLog(JoinPoint jp) {
 		
 		logger.info("-----------------------");
 		logger.info("-----------------------");
 		
+		//전달되는 모든 파라미터를  Object배열로 받아서 출력
+		logger.info(Arrays.toString(jp.getArgs()));
+		
+		//해당 Advice의 타입을 출력
+//		logger.info(jp.getKind());
+		//실행하는 대상객체의 method정보를 출력
+//		logger.info(jp.getSignature().toLongString());
+		//target객체의 type이름을 출력
+//		logger.info(jp.getTarget().getClass().getTypeName());
+		//Advice를 행하는 객체의 type이름을 출력
+//		logger.info(jp.getThis().getClass().getTypeName());
 	}
 	
 }
