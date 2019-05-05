275a8950fec27be09a524c5cbb59952b933b82b7
Seungyeop Lee <xbeast7@gmail.com>
2019-05-05 22:37:11 +0900

    Part5 Chapter4.1 파일 데이터 전송하기
    Part5 게시물의 첨부파일 기능
    Chapter4 전송된 파일을 화면에 표시하기

--- ex04/src/main/java/org/zerock/controller/UploadController.java
+++ ex04/src/main/java/org/zerock/controller/UploadController.java
@@ -1,13 +1,18 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.UUID;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.util.MediaUtils;
 import org.zerock.util.UploadFileUtils;
 
 @Controller
@@ -74,6 +80,34 @@ public class UploadController {
 				UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes()), 
 				HttpStatus.CREATED);
 		
+	}
+	
+	@ResponseBody
+	@RequestMapping("/displayFile")
+	public ResponseEntity<byte[]> displayFile(String fileName) throws Exception {
+		
+		logger.info("FILE NAME : " + fileName);
+		
+		HttpHeaders headers = new HttpHeaders();
+		String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
+		MediaType mType = MediaUtils.getMediaType(formatName);
+		if(mType != null) {
+			headers.setContentType(mType);
+		} else {
+			String rawFileName = fileName.substring(fileName.indexOf("_") + 1);
+			headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);	//다운로드 용 MIME타입
+			headers.add("Content-Disposition", "attachment; filename=\"" + new String(rawFileName.getBytes("UTF-8"), "ISO-8859-1") + "\"");
+		}
+		
+		ResponseEntity<byte[]> entity = null;
+		try(InputStream in = new FileInputStream(uploadPath + fileName)) {
+			entity = new ResponseEntity<>(IOUtils.toByteArray(in), headers, HttpStatus.CREATED);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		return entity;
 	}
 	
 }
