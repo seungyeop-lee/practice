03501be6e2677dc61400583666ae43ad62898fa0
Seungyeop Lee <xbeast7@gmail.com>
2019-05-06 09:18:17 +0900

    Part5 Chapter5.1 UploadController의 삭제 처리
    Part5 게시물의 첨부파일 기능
    Chapter5 첨부파일의 삭제

--- ex04/src/main/java/org/zerock/controller/UploadController.java
+++ ex04/src/main/java/org/zerock/controller/UploadController.java
@@ -110,4 +110,26 @@ public class UploadController {
 		return entity;
 	}
 	
+	@ResponseBody
+	@RequestMapping(value = "/deleteFile", method = RequestMethod.POST)
+	public ResponseEntity<String> deleteFile(String fileName) {
+		
+		logger.info("delete file: " + fileName);
+		
+		String formatName = fileName.substring(fileName.lastIndexOf(".") + 1);
+		MediaType mType = MediaUtils.getMediaType(formatName);
+		
+		//이미지 파일인 경우 썸네일 삭제
+		if(mType != null) {
+			String front = fileName.substring(0, 12);
+			String end = fileName.substring(14);
+			new File(uploadPath + (front + end).replace('/', File.separatorChar)).delete();
+		}
+		
+		//저장된 원본 파일 삭제
+		new File(uploadPath + fileName.replace('/', File.separatorChar)).delete();
+		
+		return new ResponseEntity<>("deleted", HttpStatus.OK);
+	}
+	
 }
