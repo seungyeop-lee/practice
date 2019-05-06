134c8a22a4a2875fe833bea670f204aefbf62f25
Seungyeop Lee <xbeast7@gmail.com>
2019-05-07 00:15:20 +0900

    Part6 Chapter4.3 로그아웃 처리
    Part6 인터셉터(Interceptor)를 활용하는 로그인 처리
    Chapter4 자동 로그인과 쿠키

--- ex05/src/main/java/org/zerock/controller/UserController.java
+++ ex05/src/main/java/org/zerock/controller/UserController.java
@@ -4,6 +4,9 @@ import java.util.Date;
 import java.util.Objects;
 
 import javax.inject.Inject;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
@@ -11,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.util.WebUtils;
 import org.zerock.domain.UserVO;
 import org.zerock.dto.LoginDTO;
 import org.zerock.service.UserService;
@@ -43,5 +47,30 @@ public class UserController {
 			Date sessionLimit = new Date(System.currentTimeMillis() + (1000 * amount));
 			service.keepLogin(vo.getUid(), session.getId(), sessionLimit);
 		}
+	}
+	
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logout(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws Exception {
+		
+		Object obj = session.getAttribute("login");
+		
+		if(obj != null) {
+			UserVO vo = (UserVO) obj;
+			
+			session.removeAttribute("login");
+			session.invalidate();
+			
+			Cookie loginCookie = WebUtils.getCookie(request, "loginCookie");
+			
+			if(loginCookie != null) {
+				loginCookie.setPath("/");
+				loginCookie.setMaxAge(0);
+				response.addCookie(loginCookie);
+				service.keepLogin(vo.getUid(), session.getId(), new Date());
+			}
+		}
+		
+		return "user/logout";
+		
 	}
 }
