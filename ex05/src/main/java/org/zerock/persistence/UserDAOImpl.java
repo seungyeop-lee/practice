b18e658b263187dfe62e42609a97461124323ec0
Seungyeop Lee <xbeast7@gmail.com>
2019-05-06 16:28:41 +0900

    Part6 Chapter2.1 준비 작업
    Part6 인터셉터(Interceptor)를 활용하는 로그인 처리
    Chapter2 HttpSession을 이용하는 로그인 처리

---
+++ ex05/src/main/java/org/zerock/persistence/UserDAOImpl.java
@@ -0,0 +1,23 @@
+package org.zerock.persistence;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+import org.zerock.domain.UserVO;
+import org.zerock.dto.LoginDTO;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+	@Inject
+	private SqlSession session;
+	
+	private static String namespace = "org.zerock.mapper.UserMapper";
+	
+	@Override
+	public UserVO login(LoginDTO dto) throws Exception {
+		return session.selectOne(namespace + ".login", dto);
+	}
+
+}
---
+++ ex05/src/main/resources/dml.sql
@@ -0,0 +1,5 @@
+insert into tbl_user(uid, upw, uname) values ('user00', 'user00', 'IRON MAN');
+insert into tbl_user(uid, upw, uname) values ('user01', 'user01', 'CAPTAIN');
+insert into tbl_user(uid, upw, uname) values ('user02', 'user02', 'HULK');
+insert into tbl_user(uid, upw, uname) values ('user03', 'user03', 'Thor');
+insert into tbl_user(uid, upw, uname) values ('user10', 'user10', 'Quick Silver');
