ec20fbb1ac845525bad4f293dd7aaddf7d4b852f
Seungyeop Lee <xbeast7@gmail.com>
2019-05-08 22:29:42 +0900

    부록1.4 Mapper의 테스트
    부록1 MyBatis의 Mapper 인터페이스 활용

---
+++ app1/src/test/java/org/zerock/test/SampleMapperTest.java
@@ -0,0 +1,26 @@
+package org.zerock.test;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.persistence.SampleMapper;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class SampleMapperTest {
+
+	@Inject
+	private SampleMapper mapper;
+	
+	@Test
+	public void testTime() {
+		
+		System.out.println(mapper.getClass().getName());	//com.sun.proxy.$Proxy14
+		
+		System.out.println(mapper.getTime());	//2019-05-08 22:28:13
+		
+	}
+}
