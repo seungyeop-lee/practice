81048ec93d1cb3dfb846b9235e03c0ab721bd19a
Seungyeop Lee <xbeast7@gmail.com>
2019-05-09 22:36:47 +0900

    부록2.3 스프링 부트에서의 데이터베이스 설정
    부록2 스프링 부트를 이용한 프로젝트 생성

--- app2/src/test/java/org/zerock/App2ApplicationTests.java
+++ app2/src/test/java/org/zerock/App2ApplicationTests.java
@@ -1,7 +1,12 @@
 package org.zerock;
 
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +14,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class App2ApplicationTests {
 
+	@Autowired
+	private DataSource ds;
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void testConnection() throws Exception {
+		
+		System.out.println(ds);
+		
+		Connection con = ds.getConnection();
+		
+		System.out.println(con);
+		
+		con.close();
+		
 	}
 
 }
