0198d11c71e9c49cb3374495eb2efd6190e5b848
Seungyeop Lee <xbeast7@gmail.com>
2019-05-11 23:19:02 +0900

    1.2.2 커넥션 만들기의 추출
    1장 오브젝트와 의존관계
    1.2 DAO의 분리

--- chap01/src/main/java/springbook/user/dao/UserDao.java
+++ chap01/src/main/java/springbook/user/dao/UserDao.java
@@ -12,8 +12,7 @@ public class UserDao {
 
 	public void add(User user) throws ClassNotFoundException, SQLException {
 		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/springbook?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Tokyo", "spring", "book");
+		Connection c = getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
 		ps.setString(1, user.getId());
@@ -29,8 +28,7 @@ public class UserDao {
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
 		
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/springbook?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Tokyo", "spring", "book");
+		Connection c = getConnection();
 
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
@@ -49,6 +47,11 @@ public class UserDao {
 		
 		return user;
 		
+	}
+	
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection("jdbc:mysql://localhost:3306/springbook?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Tokyo", "spring", "book");
 	}
 	
 }
