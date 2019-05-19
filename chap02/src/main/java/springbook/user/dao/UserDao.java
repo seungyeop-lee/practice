1308986d5588b75e102a118d6bd604e3843dabb7
Seungyeop Lee <xbeast7@gmail.com>
2019-05-19 23:35:45 +0900

    2.3.2 테스트 결과의 일관성
    2장 테스트
    2.3 개발자를 위한 테스팅 프레임워크 JUnit

--- chap02/src/main/java/springbook/user/dao/UserDao.java
+++ chap02/src/main/java/springbook/user/dao/UserDao.java
@@ -56,4 +56,35 @@ public class UserDao {
 		
 	}
 	
+	public void deleteAll() throws SQLException {
+		
+		Connection c = dataSource.getConnection();
+		
+		PreparedStatement ps = c.prepareStatement("delete from users");
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+		
+	}
+	
+	public int getCount() throws SQLException {
+		
+		Connection c = dataSource.getConnection();
+		
+		PreparedStatement ps = c.prepareStatement("select count(*) from users");
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		int count = rs.getInt(1);
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return count;
+		
+	}
+	
 }
--- chap02/src/test/java/springbook/user/dao/UserDaoTest.java
+++ chap02/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertThat;
 import java.sql.SQLException;
 
 import org.junit.Test;
-import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -21,13 +20,16 @@ public class UserDaoTest {
 		
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
+		dao.deleteAll();	//테스트 전 DB의 데이터를 전부 삭제
+		assertThat(dao.getCount(), is(0));	//dao.deleteAll()작동 확인
+		
 		User user = new User();
 		user.setId("xbeast7");
 		user.setName("이승엽");
 		user.setPassword("single");
 		
 		dao.add(user);
-		System.out.println(user.getId() + " 등록 성공");
+		assertThat(dao.getCount(), is(1));	//dao.getCount()작동 확인
 		
 		User user2 = dao.get(user.getId());
 		assertThat(user.getName(), is(user2.getName()));
