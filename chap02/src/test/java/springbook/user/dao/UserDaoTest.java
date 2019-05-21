0ecb3561bd6d222a46a44b73f7cae8e89fa724a0
Seungyeop Lee <xbeast7@gmail.com>
2019-05-21 21:36:18 +0900

    2.4.2 DI와 테스트 - 테스트를 위한 별도의 DI 설정
    2장 테스트
    2.4 스프링 테스트 적용

--- chap02/src/test/java/springbook/user/dao/UserDaoTest.java
+++ chap02/src/test/java/springbook/user/dao/UserDaoTest.java
@@ -5,15 +5,11 @@ import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
 
-import javax.sql.DataSource;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -22,10 +18,7 @@ import springbook.user.domain.User;
 //스프링의 요소를 테스트에서 사용 가능하게 함
 @RunWith(SpringJUnit4ClassRunner.class)
 //구성xml파일 위치 설정, 다른 테스트 클래스에 동일한 설정이 있을경우 동일한 애플리케이션 컨텍스트를 공유
-@ContextConfiguration(locations="/applicationContext.xml")
-//애플리케이션 컨텍스트의 구성이나 상태를 변경하는 것을 명시함으로써, 테스트가 끝난 후 애플리케이션 컨텍스트를 새로 생성
-//메소드 레벨에도 사용 할 수 있으므로, 가능하면 메소드 레벨에 사용하여 더 좁은 범위에 대해 애플리케이션 컨텍스트가 새로 생성되게 하는 것이 좋음
-@DirtiesContext
+@ContextConfiguration(locations="/test-applicationContext.xml")
 public class UserDaoTest {
 
 	//픽스처(테스트를 수행하는 데 필요한 정보나 오브젝트)
@@ -39,14 +32,6 @@ public class UserDaoTest {
 	//각 테스트를 실행하기 전, 테스트에 필요한 환경 구축
 	@Before
 	public void setUp() {
-		//Spring에서 제공하는 가장 빠른 DataSource
-		//커넥션이 하나만 만들어지므로, 테스트에서만 사용!
-		DataSource dataSource = new SingleConnectionDataSource(
-				"jdbc:mysql://localhost:3306/testdb?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Tokyo",
-				"spring", "book", true);
-		//DataSource를 dao에 주입 
-		dao.setDataSource(dataSource);
-		
 		user1 = new User("id1111", "name1111", "ps1111");
 		user2 = new User("id2222", "name2222", "ps2222");
 		user3 = new User("id3333", "name3333", "ps3333");
