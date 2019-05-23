10caba15d559cc196975a3c12707593bc6095b43
Seungyeop Lee <xbeast7@gmail.com>
2019-05-23 22:31:52 +0900

    3.5.3 템플릿/콜백의 응용 - 중복의 제거와 템플릿/콜백 설계
    3장 템플릿
    3.5 템플릿과 콜백

--- chap03/src/test/java/springbook/learningtest/template/CalcSumTest.java
+++ chap03/src/test/java/springbook/learningtest/template/CalcSumTest.java
@@ -5,21 +5,30 @@ import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalcSumTest {
 	
+	Calculator calculator;
+	String numFilepath;
+	
+	@Before
+	public void setUp() {
+		this.calculator = new Calculator();
+		this.numFilepath = getClass().getResource("numbers.txt").getPath();
+	}
+	
 	//파일의 숫자 합을 계산하는 테스트 메소드
 	@Test
 	public void sumOfNumbers() throws IOException {
-		
-		Calculator calculator = new Calculator();
-		int sum = calculator.calcSum(
-				getClass()	//현재 클래스의 정보를 가진 객체 획득
-				.getResource("numbers.txt")	//클래스의 위치에 numbers.txt 파일의 정보를 가진 객체 획득
-				.getPath());	//numbers.txt 파일의 경로 획득
-		
-		assertThat(sum, is(10));
+		assertThat(calculator.calcSum(this.numFilepath), is(10));
+	}
+	
+	//파일의 숫자 곱을 계산하는 테스트 메소드
+	@Test
+	public void multiplyOfNumbers() throws IOException {
+		assertThat(calculator.calcMultiply(this.numFilepath), is(24));
 	}
 	
 }
--- chap03/src/test/java/springbook/learningtest/template/Calculator.java
+++ chap03/src/test/java/springbook/learningtest/template/Calculator.java
@@ -15,18 +15,62 @@ public class Calculator {
 	 */
 	public int calcSum(String filepath) throws IOException {
 		
+		BufferedReaderCallback sumCallback = new BufferedReaderCallback() {
+			@Override
+			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+				Integer sum = 0;
+				String line = null;
+				while((line = br.readLine()) != null) {
+					sum += Integer.valueOf(line);
+				}
+				return sum;
+			}
+		};
+		return fileReadTemplate(filepath, sumCallback);
+		
+	}
+	
+	/**
+	 * 파일의 모든 줄에 써있는 숫자를 곱함
+	 * 
+	 * @param filepath 대상 파일의 경로 
+	 * @return 모든 줄에 써있는 숫자의 곱
+	 * @throws IOException
+	 */
+	public Integer calcMultiply(String filepath) throws IOException {
+		BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
+			@Override
+			public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+				Integer multiply = 1;
+				String line = null;
+				while((line = br.readLine()) != null) {
+					multiply *= Integer.valueOf(line);
+				}
+				return multiply;
+			}
+		};
+		return fileReadTemplate(filepath, multiplyCallback);
+	}
+	
+	/**
+	 * 파일을 읽어서 callback메소드를 실행
+	 * 
+	 * @param filepath 파일 경로
+	 * @param callback 실행 메소드를 담은 객체
+	 * @return callback실행 결과
+	 * @throws IOException
+	 */
+	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+		
 		BufferedReader br = null;
 		
 		try {
 			br = new BufferedReader(new FileReader(filepath));
 			
-			Integer sum = 0;
-			String line = null;
-			while((line = br.readLine()) != null) {
-				sum += Integer.valueOf(line);
-			}
+			//변하는 부분인 계산 부분을 콜백으로 분리하여 위임한다.
+			int ret = callback.doSomethingWithReader(br);
 			
-			return sum;
+			return ret;
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 			throw e;
