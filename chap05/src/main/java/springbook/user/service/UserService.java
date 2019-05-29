2e1616107f3e3252129ae466b2422f8c3b7164b5
Seungyeop Lee <xbeast7@gmail.com>
2019-05-29 23:28:43 +0900

    5.1.5 코드 개선 - upgradeLevels() 리팩토링(1)
    5장 서비스 추상화
    5.1 사용자 레벨 관리 기능 추가

--- chap05/src/main/java/springbook/user/service/UserService.java
+++ chap05/src/main/java/springbook/user/service/UserService.java
@@ -17,24 +17,40 @@ public class UserService {
 	public void upgradeLevels() {
 		List<User> users = userDao.getAll();
 		for(User user : users) {
-			
-			boolean changed;	//레벨 변경 유무
-			if(user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-				user.setLevel(Level.SILVER);
-				changed = true;
-			} else if(user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-				user.setLevel(Level.GOLD);
-				changed = true;
-			} else if(user.getLevel() == Level.GOLD) {
-				changed = false;
-			} else {
-				changed = false;
-			}
-			
-			if(changed) {
-				userDao.update(user);
+			if(canUpgradeLevel(user)) {
+				upgradeLevel(user);
 			}
 		}
+	}
+
+	//레벨 상향 대상유무 확인
+	private boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel();
+		
+		//현재 레벨에 따른 조건 확인 및 결과 반환
+		switch (currentLevel) {
+		case BASIC:
+			return user.getLogin() >= 50;
+		case SILVER:
+			return user.getRecommend() >= 30;
+		case GOLD:
+			return false;
+		default:
+			throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+		}
+	}
+	
+	//레벨 상향 작업
+	private void upgradeLevel(User user) {
+		Level currentLevel = user.getLevel();
+		
+		if(currentLevel == Level.BASIC) {
+			user.setLevel(Level.SILVER);
+		} else if(currentLevel == Level.SILVER) {
+			user.setLevel(Level.GOLD);
+		}
+		
+		userDao.update(user);
 	}
 
 	public void add(User user) {
