b0bc81254eb4700aeb7b199074cc56fde430723f
Seungyeop Lee <xbeast7@gmail.com>
2019-06-02 12:47:41 +0900

    5.4.1 JavaMail을 이용한 메일 발송 기능 - JavaMail 메일 발송
    5장 서비스 추상화
    5.4 메일 서비스 추상화

--- chap05/src/main/java/springbook/user/service/CommonUserLevelUpgradePolicy.java
+++ chap05/src/main/java/springbook/user/service/CommonUserLevelUpgradePolicy.java
@@ -3,6 +3,16 @@ package springbook.user.service;
 import static springbook.user.service.UserService.MIN_LOGCOUNT_FOR_SILVER;
 import static springbook.user.service.UserService.MIN_RECCOMEND_FOR_GOLD;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
@@ -42,6 +52,33 @@ public class CommonUserLevelUpgradePolicy implements UserLevelUpgradePolicy {
 	public void upgradeLevel(User user) {
 		user.upgradeLevel();	//User객체 내부 데이터 수정 작업은 User객체에게 위임
 		userDao.update(user);
+		sendUpgradeEMail(user);
+	}
+
+	//레벨 상향 안내메일 발송
+	private void sendUpgradeEMail(User user) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "mail.ksug.org");
+		Session s = Session.getInstance(props);
+		
+		MimeMessage message = new MimeMessage(s);
+		try {
+			//발신인 설정
+			message.setFrom(new InternetAddress("useradmin@ksug.org"));
+			//수신인 설정
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+			//제목
+			message.setSubject("Upgrade 안내");
+			//내용
+			message.setText("사용자님의 등급이 " + user.getLevel().name() + "로 업그레이드되었습니다.");
+			
+			//전송
+			Transport.send(message);
+		} catch (AddressException e) {
+			throw new RuntimeException(e);
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
