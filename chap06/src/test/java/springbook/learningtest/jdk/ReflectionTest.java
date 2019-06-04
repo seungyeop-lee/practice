f6240f0052571550c7743f6c4e5b4f81b119592b
Seungyeop Lee <xbeast7@gmail.com>
2019-06-04 20:28:54 +0900

    6.3.2 다이내믹 프록시 - 프록시 클래스
    6장 AOP
    6.3 다이내믹 프록시와 팩토리 빈

--- chap06/src/test/java/springbook/learningtest/jdk/ReflectionTest.java
+++ chap06/src/test/java/springbook/learningtest/jdk/ReflectionTest.java
@@ -27,4 +27,19 @@ public class ReflectionTest {
 		assertThat((Character)charAtMethod.invoke(name, 0), is('S'));
 	}
 	
+	@Test
+	public void simpleProxy() {
+		
+		Hello hello = new HelloTarget();
+		assertThat(hello.sayHello("Toby"), is("Hello Toby"));
+		assertThat(hello.sayHi("Toby"), is("Hi Toby"));
+		assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
+		
+		Hello proxiedHello = new HelloUppercase(hello);
+		assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
+		assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
+		assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
+		
+	}
+	
 }
