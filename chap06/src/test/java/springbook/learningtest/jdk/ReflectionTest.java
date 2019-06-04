b4714f63c42e39aba37edca1d7d4fd5563d039d0
Seungyeop Lee <xbeast7@gmail.com>
2019-06-04 20:13:05 +0900

    6.3.2 다이내믹 프록시 - 리플렉션
    6장 AOP
    6.3 다이내믹 프록시와 팩토리 빈

---
+++ chap06/src/test/java/springbook/learningtest/jdk/ReflectionTest.java
@@ -0,0 +1,30 @@
+package springbook.learningtest.jdk;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.lang.reflect.Method;
+
+import org.junit.Test;
+
+
+public class ReflectionTest {
+	
+	@Test
+	public void invokeMethod() throws Exception {
+		String name = "Spring";
+		
+		//String.length()
+		assertThat(name.length(), is(6));
+		
+		Method lengthMethod = String.class.getMethod("length");
+		assertThat((Integer)lengthMethod.invoke(name), is(6));
+		
+		//String.charAt()
+		assertThat(name.charAt(0), is('S'));
+		
+		Method charAtMethod = String.class.getMethod("charAt", int.class);	//(메소드 이름, 파라미터 타입)
+		assertThat((Character)charAtMethod.invoke(name, 0), is('S'));
+	}
+	
+}
