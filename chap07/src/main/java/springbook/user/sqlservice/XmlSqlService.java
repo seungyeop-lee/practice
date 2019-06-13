d747aaac2788f0499e0fa8c81a1baccbcee72d60
Seungyeop Lee <xbeast7@gmail.com>
2019-06-13 22:07:01 +0900

    7.2.2 XML 파일을 이용하는 SQL 서비스
    7장 스프링 핵심기술의 응용
    7.2 인터페이스의 분리와 자기 참조 빈

---
+++ chap07/src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -0,0 +1,50 @@
+package springbook.user.sqlservice;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import springbook.user.dao.UserDao;
+import springbook.user.sqlservice.jaxb.SqlMap;
+import springbook.user.sqlservice.jaxb.SqlType;
+
+public class XmlSqlService implements SqlService {
+
+	private Map<String, String> sqlMap = new HashMap<>(); 
+	
+	public XmlSqlService() {
+		
+		String contextPath = SqlMap.class.getPackage().getName();
+		try {
+			JAXBContext context = JAXBContext.newInstance(contextPath);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+			SqlMap unmarshaledSqlMap = (SqlMap)unmarshaller.unmarshal(is);
+			
+			for(SqlType sql : unmarshaledSqlMap.getSql()) {
+				sqlMap.put(sql.getKey(), sql.getValue());
+			}
+		} catch (JAXBException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	@Override
+	public String getSql(String key) throws SqlRetrievalFailureException {
+		
+		String sql = sqlMap.get(key);
+		
+		if(sql == null) {
+			throw new SqlRetrievalFailureException(key + "에 대한 SQL을 찾을 수 없습니다.");
+		} else {
+			return sql;
+		}
+		
+	}
+
+}
