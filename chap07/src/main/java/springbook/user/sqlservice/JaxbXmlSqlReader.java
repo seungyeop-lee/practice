60f3359e32fe34fb8ff1ce21d715367e8377a221
Seungyeop Lee <xbeast7@gmail.com>
2019-06-17 21:57:24 +0900

    7.2.6 디폴트 의존관계 - 디폴트 의존관계를 갖는 빈 만들기
    7장 스프링 핵심기술의 응용
    7.2 인터페이스의 분리와 자기 참조 빈

--- chap07/src/main/java/springbook/user/sqlservice/JaxbXmlSqlReader.java
+++ chap07/src/main/java/springbook/user/sqlservice/JaxbXmlSqlReader.java
@@ -12,7 +12,9 @@ import springbook.user.sqlservice.jaxb.SqlType;
 
 public class JaxbXmlSqlReader implements SqlReader {
 
-	private String sqlmapFile;
+	private static final String DEFAULT_SQLMAP_FILE = "sqlmap.xml";
+	
+	private String sqlmapFile = DEFAULT_SQLMAP_FILE;
 	
 	public void setSqlmapFile(String sqlmapFile) {
 		this.sqlmapFile = sqlmapFile;
