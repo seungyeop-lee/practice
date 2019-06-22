f8e288840891f12a8b5c3b03a0c62fd24b2fe739
Seungyeop Lee <xbeast7@gmail.com>
2019-06-22 15:31:00 +0900

    7.5.2 내장형 데이터베이스를 이용한 SQL 레지스트리 만들기 - 내장형 DB를 이용한 SqlRegistry 만들기
    7장 스프링 핵심기술의 응용
    7.5 DI를 이용해 다양한 구현 방법 적용하기

--- chap07/src/main/java/springbook/user/sqlservice/exception/SqlNotFoundException.java
+++ chap07/src/main/java/springbook/user/sqlservice/exception/SqlNotFoundException.java
@@ -7,5 +7,9 @@ public class SqlNotFoundException extends RuntimeException {
 	public SqlNotFoundException(String message) {
 		super(message);
 	}
+
+	public SqlNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
 	
 }
