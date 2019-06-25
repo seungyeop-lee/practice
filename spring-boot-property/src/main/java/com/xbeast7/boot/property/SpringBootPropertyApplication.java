fa45989da3c7d1a9ed78ea187e39386be5604270
Seungyeop Lee <xbeast7@gmail.com>
2019-06-25 22:42:57 +0900

    2.4 환경 프로퍼티 파일 설정하기 - 실습 프로젝트 생성
    2장 스프링 부트 환경 설정

---
+++ spring-boot-property/src/main/java/com/xbeast7/boot/property/SpringBootPropertyApplication.java
@@ -0,0 +1,13 @@
+package com.xbeast7.boot.property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPropertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPropertyApplication.class, args);
+    }
+
+}
