afb4e3237099b56380939f82793bb13b6cf69129
Seungyeop Lee <xbeast7@gmail.com>
2019-08-01 15:17:36 +0900

    6-2 서비스 - todoStorage 2: remove(), add()

--- controllers.js
+++ controllers.js
@@ -3,27 +3,11 @@ angular.module('todo').controller('TodoCtrl', function($scope, todoStorage) {
     $scope.todos = todoStorage.get();
 
     $scope.remove = function(todo) {
-        // todos에 있는 todo(item)과 클릭된 부분의 todo(todo) 중 같은 부분의 index저장
-        var idx = $scope.todos.findIndex(function(item) {
-            return item == todo;
-        });
-
-        // 해당하는 todo가 있을 경우 삭제
-        if(idx > -1) {
-            $scope.todos.splice(idx, 1);
-        }
+        todoStorage.remove(todo);
     };
 
     $scope.add = function(newTodoTitle) {
-        // 새로운 todo를 생성
-        var newTodo = {
-            title: newTodoTitle,
-            completed: false,
-            createdAt: Date.now()
-        };
-
-        // todos에 추가
-        $scope.todos.push(newTodo);
+        todoStorage.add(newTodoTitle);
 
         // input태그 비워 줌
         $scope.newTodoTitle = "";
--- service.js
+++ service.js
@@ -17,8 +17,33 @@ angular.module('todo').factory('todoStorage', function() {
                 createdAt: Date.now()
             }
         ],
+
         get: function() {
             return this.todos;
+        },
+
+        remove: function(todo) {
+            // todos에 있는 todo(item)과 클릭된 부분의 todo(todo) 중 같은 부분의 index저장
+            var idx = this.todos.findIndex(function(item) {
+                return item == todo;
+            });
+
+            // 해당하는 todo가 있을 경우 삭제
+            if(idx > -1) {
+                this.todos.splice(idx, 1);
+            }
+        },
+        
+        add: function(newTodoTitle) {
+            // 새로운 todo를 생성
+            var newTodo = {
+                title: newTodoTitle,
+                completed: false,
+                createdAt: Date.now()
+            };
+
+            // todos에 추가
+            this.todos.push(newTodo);
         }
     }
 
