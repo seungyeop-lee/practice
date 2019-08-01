3306dc3115841a21c0d066bf96e8470ab6f95283
Seungyeop Lee <xbeast7@gmail.com>
2019-08-01 12:45:48 +0900

    2-3 투두 출력하기

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-//즉시 실행함수로 angular module을 실행
 (function() {
-    //todo 라는 모듈 생성
     var app = angular.module('todo', []);
 
-    //모듈에 TodoCtrl라는 컨트롤러를 추가
     app.controller('TodoCtrl', ['$scope', function($scope) {
-        $scope.name = 'Chris'
         $scope.todo = {
-            title: '요가수련',
+            title: '요가수행',
             completed: false,
             createdAt: Date.now()
         };
