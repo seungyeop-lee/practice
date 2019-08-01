e9436f565edf7ce7cd0de4b00bfaf71a704d778d
Seungyeop Lee <xbeast7@gmail.com>
2019-08-01 14:27:12 +0900

    5-1 디렉티브 - todoTitle

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
-(function() {
-    var app = angular.module('todo', []);
+var app = angular.module('todo', []);
 
-    app.controller('TodoCtrl', function($scope) {
-        $scope.todos = [
-            {
-                title: '요가수행',
-                completed: false,
-                createdAt: Date.now()
-            },
-            {
-                title: '앵귤러 학습',
-                completed: false,
-                createdAt: Date.now()
-            },
-            {
-                title: '운동하기',
-                completed: true,
-                createdAt: Date.now()
-            }
-        ];
+app.controller('TodoCtrl', function($scope) {
+    $scope.todos = [
+        {
+            title: '요가수행',
+            completed: false,
+            createdAt: Date.now()
+        },
+        {
+            title: '앵귤러 학습',
+            completed: false,
+            createdAt: Date.now()
+        },
+        {
+            title: '운동하기',
+            completed: true,
+            createdAt: Date.now()
+        }
+    ];
 
-        $scope.remove = function(todo) {
-            // todos에 있는 todo(item)과 클릭된 부분의 todo(todo) 중 같은 부분의 index저장
-            var idx = $scope.todos.findIndex(function(item) {
-                return item == todo;
-            });
+    $scope.remove = function(todo) {
+        // todos에 있는 todo(item)과 클릭된 부분의 todo(todo) 중 같은 부분의 index저장
+        var idx = $scope.todos.findIndex(function(item) {
+            return item == todo;
+        });
 
-            // 해당하는 todo가 있을 경우 삭제
-            if(idx > -1) {
-                $scope.todos.splice(idx, 1);
-            }
+        // 해당하는 todo가 있을 경우 삭제
+        if(idx > -1) {
+            $scope.todos.splice(idx, 1);
+        }
+    };
+
+    $scope.add = function(newTodoTitle) {
+        // 새로운 todo를 생성
+        var newTodo = {
+            title: newTodoTitle,
+            completed: false,
+            createdAt: Date.now()
         };
 
-        $scope.add = function(newTodoTitle) {
-            // 새로운 todo를 생성
-            var newTodo = {
-                title: newTodoTitle,
-                completed: false,
-                createdAt: Date.now()
-            };
+        // todos에 추가
+        $scope.todos.push(newTodo);
 
-            // todos에 추가
-            $scope.todos.push(newTodo);
+        // input태그 비워 줌
+        $scope.newTodoTitle = "";
+    };
+});
 
-            // input태그 비워 줌
-            $scope.newTodoTitle = "";
-        };
-    });
-})();
+//directive로 설정 할 경우, html에서 태그로 사용 가능
+app.directive('todoTitle', function() {
+    return {
+        template: '<h1>Todo 목록</h1>'
+    };
+});
