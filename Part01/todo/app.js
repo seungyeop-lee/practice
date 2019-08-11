db87aef82fc540d0e30cf54fd7c823ee5aae50ee
Seungyeop Lee <xbeast7@gmail.com>
2019-08-11 23:14:57 +0900

    TODO App 나머지 기능 구현
    TODO 웹 애플리케이션

--- Part01/todo/app.js
+++ Part01/todo/app.js
@@ -9,4 +9,24 @@ var myApp = angular.module('myApp', []);
 myApp.controller('todoCtrl', function($scope) {
     $scope.appName = "AngularJS TODO APP";
     $scope.todoList = todoList;
+    $scope.addNewTodo = function(newTitle) {
+        todoList.push({ done : false, title : newTitle});
+        $scope.newTitle = '';
+    };
+    $scope.archive = function() {
+        for(var i = $scope.todoList.length - 1; i >= 0; i--) {
+            if($scope.todoList[i].done) {
+                $scope.todoList.splice(i, 1);
+            }
+        }
+    };
+    $scope.remain = function() {
+        var remainCount = 0;
+        angular.forEach($scope.todoList, function(value, key) {
+            if(value.done === false) {
+                remainCount++;
+            }
+        });
+        return remainCount;
+    };
 });
