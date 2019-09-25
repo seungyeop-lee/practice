70ab9edbd59b9d9b070394d84cb06156ae6aca93
Seungyeop Lee <xbeast7@gmail.com>
2019-09-25 23:33:53 +0900

    책과 동일한 테스트 코드위치 및 구성으로 수정

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -2,7 +2,14 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class FrancTest {
+public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+
     @Test
     public void testFrancMultiplication() {
         Franc five = new Franc(5);
@@ -12,8 +19,9 @@ public class FrancTest {
 
     @Test
     public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
     }
-
 }
