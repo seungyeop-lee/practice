dabe299b9ab38e22bf84c31b1016a968d6b67341
Seungyeop Lee <xbeast7@gmail.com>
2019-09-26 00:00:16 +0900

    1.8 객체 만들기
    1 화폐 예제

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,5 +1,14 @@
-public class Money {
+public abstract class Money {
     protected int amount;
+    abstract Money times(int multiplier);
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Franc franc(int amount) {
+        return new Franc(amount);
+    }
 
     @Override
     public boolean equals(Object obj) {
