3a872139e47aa998ed66f4f02a30e4b41c4a7e13
Seungyeop Lee <xbeast7@gmail.com>
2019-09-28 19:01:28 +0900

    2.19 테이블 차리기
    2 xUnit 예시

--- part2/src/test/TestCaseTest.py
+++ part2/src/test/TestCaseTest.py
@@ -3,11 +3,17 @@ from src.main.WasRun import WasRun
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun('testMethod')
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+        self.test.run()
+        assert self.test.wasRun
+
+    def testSetUp(self):
+        self.test.run()
+        assert self.test.wasSetUp
 
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
