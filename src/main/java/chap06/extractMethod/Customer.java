846999e7f70c8acc50ef5fd8e6dc38b22a18559a
Seungyeop Lee <xbeast7@gmail.com>
2019-11-11 22:55:23 +0900

    메서드 추출 - 지역변수 사용 안 함
    chap06 메서드 정리

--- src/main/java/chap06/extractMethod/Customer.java
+++ src/main/java/chap06/extractMethod/Customer.java
@@ -16,10 +16,7 @@ public class Customer {
         Enumeration<Order> e = _orders.elements();
         double outstanding = 0.0;
 
-        // 배너 출력
-        System.out.println("*****************************");
-        System.out.println("********** 고객 외상 **********");
-        System.out.println("*****************************");
+        printBanner();
 
         // 외상액 계산
         while (e.hasMoreElements()) {
@@ -30,6 +27,13 @@ public class Customer {
         // 세부 내역 출력
         System.out.println("고객명: " + _name);
         System.out.println("외상액: " + outstanding);
+    }
+
+    void printBanner() {
+        // 배너 출력
+        System.out.println("*****************************");
+        System.out.println("********** 고객 외상 **********");
+        System.out.println("*****************************");
     }
 
 }
