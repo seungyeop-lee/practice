4bf5865cb683e68d03e605791473b8a4723a08c5
Seungyeop Lee <xbeast7@gmail.com>
2019-11-13 22:31:32 +0900

    임시변수를 메서드 호출로 전환 - 리팩토링 적용
    chap06 메서드 정리

--- src/main/java/chap06/replaceTempWithQuery/Product.java
+++ src/main/java/chap06/replaceTempWithQuery/Product.java
@@ -11,13 +11,18 @@ public class Product {
     }
 
     Double getPrice() {
-        int basePrice = _quantity * _itemPrice;
-        double discountFactor;
-        if (basePrice > 1000) {
-            discountFactor = 0.95;
+        return basePrice() * discountFactor();
+    }
+
+    private int basePrice() {
+        return _quantity * _itemPrice;
+    }
+
+    private double discountFactor() {
+        if (basePrice() > 1000) {
+            return 0.95;
         } else {
-            discountFactor = 0.98;
+            return 0.98;
         }
-        return basePrice * discountFactor;
     }
 }
