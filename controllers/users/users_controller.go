193ee23d8583e9ce07e90ef405bb58ab05da563e
seungyeop-lee <xbeast7@gmail.com>
2019-12-11 21:17:48 +0900

    6. Starting from our domain

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,11 +2,27 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/seungyeop-lee/bookstore_users-api/domain/users"
+	"github.com/seungyeop-lee/bookstore_users-api/services"
+	"github.com/seungyeop-lee/bookstore_users-api/utils/errors"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
