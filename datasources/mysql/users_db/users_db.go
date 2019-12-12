8def76a4682ec48affbd93d8460658987f056b6a
seungyeop-lee <xbeast7@gmail.com>
2019-12-12 16:30:23 +0900

    9. Configure MySQL client in Go

---
+++ datasources/mysql/users_db/users_db.go
@@ -0,0 +1,42 @@
+package users_db
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"os"
+)
+
+const (
+	mysql_users_username = "mysql_users_username"
+	mysql_users_password = "mysql_users_password"
+	mysql_users_host = "mysql_users_host"
+	mysql_users_schema = "mysql_users_schema"
+)
+
+var (
+	Client *sql.DB
+
+	username = os.Getenv(mysql_users_username)
+	password = os.Getenv(mysql_users_password)
+	host = os.Getenv(mysql_users_host)
+	schema = os.Getenv(mysql_users_schema)
+)
+
+// init 함수는 패키지가 로드될 때 가장먼저 자동으로 실행된다.
+// 패키지의 초기화 로직이 필요 할 때 선택적으로 사용한다.
+func init() {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
+		username, password,	host, schema,
+	)
+	var err error
+	Client, err = sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+	if err = Client.Ping(); err != nil {
+		panic(err)
+	}
+	log.Println("database successfully configured")
+}
