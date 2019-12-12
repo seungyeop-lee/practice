f60c483fad0ebf9a34401d17b72291d1c30ed74d
seungyeop-lee <xbeast7@gmail.com>
2019-12-12 15:55:26 +0900

    8. Working with dates

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"fmt"
+	"github.com/seungyeop-lee/bookstore_users-api/utils/date_utils"
 	"github.com/seungyeop-lee/bookstore_users-api/utils/errors"
 )
 
@@ -32,6 +33,9 @@ func (user *User) Save() *errors.RestErr {
 		}
 		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
 	}
+
+	user.DateCreated = date_utils.GetNowString()
+
 	usersDB[user.Id] = user
 	return nil
 }
