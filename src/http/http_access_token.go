c3f9a9e76dbe9ee357aec4cb1c54c4a3fd7c16ab
Seungyeop Lee <xbeast7@gmail.com>
2019-12-16 23:09:07 +0900

    25. How to insert, get and update records

--- src/http/http_access_token.go
+++ src/http/http_access_token.go
@@ -3,11 +3,13 @@ package http
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/seungyeop-lee/bookstore_oauth-api/src/domain/access_token"
+	"github.com/seungyeop-lee/bookstore_oauth-api/src/utils/errors"
 	"net/http"
 )
 
 type AccessTokenHandler interface {
 	GetById(*gin.Context)
+	Create(*gin.Context)
 }
 
 type accessTokenHandler struct {
@@ -16,7 +18,7 @@ type accessTokenHandler struct {
 
 func NewHandler(service access_token.Service) AccessTokenHandler {
 	return &accessTokenHandler{
-		service:service,
+		service: service,
 	}
 }
 
@@ -27,4 +29,19 @@ func (handler *accessTokenHandler) GetById(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, accessToken)
+}
+
+func (handler *accessTokenHandler) Create(c *gin.Context) {
+	var at access_token.AccessToken
+	if err := c.ShouldBindJSON(&at); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	if err := handler.service.Create(at); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+	c.JSON(http.StatusCreated, at)
 }
