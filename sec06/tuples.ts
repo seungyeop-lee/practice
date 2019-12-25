4e6377c933a327bfcaae2399022a30478de9a0c8
seungyeop-lee <xbeast7@gmail.com>
2019-12-25 22:58:10 +0900

    35. Why Tuples?
    Section6: Tuples in Typescript

--- sec06/tuples.ts
+++ sec06/tuples.ts
@@ -16,3 +16,12 @@ type Drink = [string, boolean, number];
 const coke: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+// 튜플은 각 데이터가 의미하는 바를 나타낼 수 없다.
+const carSpece: [number, number] = [400, 3354];
+
+// 객체는 각 데이터가 의미하는 바를 나타낼 수 있다.
+const carStats = {
+  horsepower: 400,
+  weight: 3354
+};
