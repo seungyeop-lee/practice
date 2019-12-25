49d3b2ead0789c8cd41d0b537fb1afdfbeb6f6ea
seungyeop-lee <xbeast7@gmail.com>
2019-12-25 22:01:24 +0900

    24. Inference Around Functions
    Section4: Annotations With Functions and Objects

--- sec04/annotations/functions.ts
+++ sec04/annotations/functions.ts
@@ -1,3 +1,9 @@
+// return형은 추론이 가능하다.
 const add = (a: number, b: number): number => {
   return a + b;
+};
+
+// but! 실수 방지를 위해 언제나 선언하도록 한다.
+const subtract = (a: number, b: number): number => {
+  return a - b;
 };
