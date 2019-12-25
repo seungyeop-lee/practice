b18e95542f747f2806019057b0c3ea8bc305b6b4
seungyeop-lee <xbeast7@gmail.com>
2019-12-25 21:08:27 +0900

    15. Annotations with Variables
    Section3: Type Annotations in Action

---
+++ sec02/features/annotations/variables.ts
@@ -0,0 +1,9 @@
+let apples: number = 5;
+let spped: string = 'fast';
+let hanName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
