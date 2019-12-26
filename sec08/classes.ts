b57a6fa8fb80e5509882292d271f67a06cad509b
seungyeop-lee <xbeast7@gmail.com>
2019-12-26 22:43:57 +0900

    46. Fields in Classes
    Section8: Building Functionality with Classes

--- sec08/classes.ts
+++ sec08/classes.ts
@@ -1,23 +1,31 @@
 class Vehicle {
+  // color: string = 'red';
+
+  // constructor(color: string) {
+  //   this.color = color;
+  // }
+
+  // public, private, protected를 사용하면 필드의 값으로 사용 가능!
+  constructor(public color: string) {}
+
   protected honk(): void {
     console.log('beep');
   }
 }
 
-const vehicle = new Vehicle();
-vehicle.honk();
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
 
-class Car extends Vehicle {
-  private drive(): void {
-    console.log('vroom');
-  }
+// class Car extends Vehicle {
+//   private drive(): void {
+//     console.log('vroom');
+//   }
 
-  startDrivingProcess(): void {
-    this.drive();
-    this.honk();
-  }
-}
+//   startDrivingProcess(): void {
+//     this.drive();
+//     this.honk();
+//   }
+// }
 
-const car = new Car();
-car.startDrivingProcess();
-car.honk();
+// const car = new Car();
+// car.startDrivingProcess();
