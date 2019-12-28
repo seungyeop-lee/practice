d5fd9c915800fe958a1d78577de40a45c249b72c
seungyeop-lee <xbeast7@gmail.com>
2019-12-28 12:44:49 +0900

    57. Note on Generating an API Key
    Section9: Design Patterns with Typescript

--- sec09/maps/src/index.ts
+++ sec09/maps/src/index.ts
@@ -1,5 +1,13 @@
 import { User } from './User';
 import { Company } from './Company';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const googleMapScript = window.document.getElementById('googleMapScript');
+googleMapScript.setAttribute(
+  'src',
+  `https://maps.googleapis.com/maps/api/js?key=${process.env.googleMapKey}`
+);
 
 const user = new User();
 
