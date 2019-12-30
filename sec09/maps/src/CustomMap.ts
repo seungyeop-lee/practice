cc020e77a5399f728bd1b5f9eb140279960645c5
seungyeop-lee <xbeast7@gmail.com>
2019-12-31 00:28:53 +0900

    66. Restricting Access with Interfaces
    Section9: Design Patterns with Typescript

--- sec09/maps/src/CustomMap.ts
+++ sec09/maps/src/CustomMap.ts
@@ -2,6 +2,15 @@ import 'googlemaps';
 import { User } from './User';
 import { Company } from './Company';
 
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -15,7 +24,7 @@ export class CustomMap {
     });
   }
 
-  addMarker(mappable: User | Company): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
