320c96cf764fad12a643c6db66dedb34dd1daf17
seungyeop-lee <xbeast7@gmail.com>
2019-12-31 00:15:30 +0900

    64. Duplicate Code
    Section9: Design Patterns with Typescript

--- sec09/maps/src/index.ts
+++ sec09/maps/src/index.ts
@@ -1,11 +1,12 @@
-// import { User } from './User';
-// import { Company } from './Company';
-import { CustomMap } from './CustomMap';
 import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 window.onload = () => {
   const user = new User();
+  const company = new Company();
   const customMap = new CustomMap('map');
 
   customMap.addUserMarker(user);
+  customMap.addCompanyMarker(company);
 };
