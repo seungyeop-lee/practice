70a78bff26a2725bac800957e843c51934c64ba7
seungyeop-lee <xbeast7@gmail.com>
2019-12-31 00:22:39 +0900

    65. One Possible Solution
    Section9: Design Patterns with Typescript

--- sec09/maps/src/index.ts
+++ sec09/maps/src/index.ts
@@ -7,6 +7,6 @@ window.onload = () => {
   const company = new Company();
   const customMap = new CustomMap('map');
 
-  customMap.addUserMarker(user);
-  customMap.addCompanyMarker(company);
+  customMap.addMarker(user);
+  customMap.addMarker(company);
 };
