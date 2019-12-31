d502063885a2374538cac8f9335bad65f984d0f7
seungyeop-lee <xbeast7@gmail.com>
2019-12-31 18:05:22 +0900

    70. Optional Implements Clauses
    Section9: Design Patterns with Typescript

--- sec09/maps/src/User.ts
+++ sec09/maps/src/User.ts
@@ -1,15 +1,17 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
 // 해당 파일을 import 할 경우 기본적으로 로딩되는 값은 default 키워드로 지정
 // 그러나 사용는 피하는걸 추천
 // export default 'red';
 
-export class User {
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = 'red';
 
   constructor() {
     this.name = faker.name.firstName();
