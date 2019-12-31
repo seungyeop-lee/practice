b3f41ba2acdf250ee81bb320f07ec0f2869c4812
seungyeop-lee <xbeast7@gmail.com>
2019-12-31 17:58:10 +0900

    69. Updating Interface Definitions
    Section9: Design Patterns with Typescript

--- sec09/maps/src/CustomMap.ts
+++ sec09/maps/src/CustomMap.ts
@@ -7,6 +7,7 @@ interface Mappable {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
 }
 
 export class CustomMap {
@@ -33,7 +34,7 @@ export class CustomMap {
 
     marker.addListener('click', () => {
       const infoWindow = new google.maps.InfoWindow({
-        content: 'Hi there!'
+        content: mappable.markerContent()
       });
 
       infoWindow.open(this.googleMap, marker);
