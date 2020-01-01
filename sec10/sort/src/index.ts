0d4e6bd0728b178eb3f3555d2885fdca8623dc4e
seungyeop-lee <xbeast7@gmail.com>
2020-01-01 22:26:52 +0900

    82. Extracting Key Logic
    Section10: More on Design Patterns

--- sec10/sort/src/index.ts
+++ sec10/sort/src/index.ts
@@ -1,34 +1,3 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
-
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // All of this only works if collection is number[]
-        // If collection is an array of numbers
-        // 모든 type에 대해 사용 가능
-        if (this.collection instanceof Array) {
-          // collection === number[]
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        // Only going to work if collection is a string
-        // If collection is a string, do this logic instead:
-        // ~~~logic to compare and swap characters in a string
-        // number, boolean, string, symbol에만 사용!
-        if (typeof this.collection === 'string') {
-        }
-      }
-    }
-  }
-}
-
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
