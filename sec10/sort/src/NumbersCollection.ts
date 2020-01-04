f44d55606b5400a4f6905b3c7da753316638040b
seungyeop-lee <xbeast7@gmail.com>
2020-01-04 22:44:48 +0900

    93. Why Use Abstract Classes?
    Section10: More on Design Patterns

--- sec10/sort/src/NumbersCollection.ts
+++ sec10/sort/src/NumbersCollection.ts
@@ -1,5 +1,9 @@
-export class NumbersCollection {
-  constructor(public data: number[]) {}
+import { Sorter } from './Sorter';
+
+export class NumbersCollection extends Sorter {
+  constructor(public data: number[]) {
+    super();
+  }
 
   // get 키워드를 사용하면 length를 필드값으로 가져갈 경우 해당 메소드의 return값이 반환된다.
   get length(): number {
