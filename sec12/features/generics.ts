e2fa643ea839e8b34c44b46854cbffe884eb0d89
seungyeop-lee <xbeast7@gmail.com>
2020-01-07 08:33:06 +0900

    135. Function Generics
    Section12: Advanced Generics

--- sec12/features/generics.ts
+++ sec12/features/generics.ts
@@ -24,3 +24,24 @@ class ArrayOfAnything<T> {
 
 // Generic을 생략해도 추정해준다.
 const arr = new ArrayOfAnything(['a', 'b', 'c']);
+
+// Example of generics with functions
+function printStrings(arr: string[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything(['a', 'b', 'c']);
