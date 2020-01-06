2f778f68749d1f5f123659ea20301821a04cd147
seungyeop-lee <xbeast7@gmail.com>
2020-01-07 00:01:53 +0900

    131. Oops, My Bad
    Section11: Reusable Code

--- sec11/stats/src/index.ts
+++ sec11/stats/src/index.ts
@@ -1,18 +1,8 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReport';
 
-// Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
-
-// Create an instance of MatchReader and pass in somthing satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
-
+const matchReader = MatchReader.fromCsv('football.csv');
 const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
+matchReader.load();
 summary.buildAndPrintReport(matchReader.matches);
