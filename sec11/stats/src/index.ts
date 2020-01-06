316edd51283087dab0101b4decffcbea8b60ae02
seungyeop-lee <xbeast7@gmail.com>
2020-01-06 23:57:24 +0900

    130. One Last Thing!
    Section11: Reusable Code

--- sec11/stats/src/index.ts
+++ sec11/stats/src/index.ts
@@ -13,5 +13,6 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
 summary.buildAndPrintReport(matchReader.matches);
