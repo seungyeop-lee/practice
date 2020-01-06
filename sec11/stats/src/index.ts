53d1153d4e1fbb63133e7a9a3f581f0460723c54
seungyeop-lee <xbeast7@gmail.com>
2020-01-06 23:35:09 +0900

    126. Implementing an Analyzer Class
    Section11: Reusable Code

--- sec11/stats/src/index.ts
+++ sec11/stats/src/index.ts
@@ -10,17 +10,5 @@ const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-// matchreader.matches
-
-//Analyze
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
 //Report
 console.log(`Man United won ${manUnitedWins} games`);
