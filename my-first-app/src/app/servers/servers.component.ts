bba7d18657fbf75e36ea2ee4b0986d13e7ccbf6c
seungyeop-lee <xbeast7@gmail.com>
2020-01-08 08:53:03 +0900

    26. Property Binding
    Section2: The Basics

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -5,15 +5,20 @@ import {Component, OnInit} from '@angular/core';
   // selector: '.app-servers',  // 클래스 선택자
   selector: 'app-servers',  // 태그 선택자; 꼭 이것만 쓰자!
   // 3줄 이상이면 templateUrl을 이용해 html파일로 따로 빼는 것을 추천
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
   constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
   ngOnInit() {
