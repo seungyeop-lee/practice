f93db809a9d96579754aaf951c0f23228ba8277f
seungyeop-lee <xbeast7@gmail.com>
2020-01-08 00:15:38 +0900

    20. Working with Component Templates
    Section2: The Basics

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,13 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // 3줄 이상이면 templateUrl을 이용해 html파일로 따로 빼는 것을 추천
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
