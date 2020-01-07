bed84ed711e68ae2263116ef5743f04deb1195db
seungyeop-lee <xbeast7@gmail.com>
2020-01-08 00:31:23 +0900

    22. Fully Understanding the Component Selector
    Section2: The Basics

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]', // 속성 선택자
+  // selector: '.app-servers',  // 클래스 선택자
+  selector: 'app-servers',  // 태그 선택자; 꼭 이것만 쓰자!
   // 3줄 이상이면 templateUrl을 이용해 html파일로 따로 빼는 것을 추천
   template: `
     <app-server></app-server>
