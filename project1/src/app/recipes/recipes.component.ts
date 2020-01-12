7819b4af66d91e032011d69dcfcc06dbea5851d2
seungyeop-lee <xbeast7@gmail.com>
2020-01-12 09:24:36 +0900

    51. Creating a "Recipe" Model
    Section3: Course Project - The Basics

--- project1/src/app/recipes/recipes.component.ts
+++ project1/src/app/recipes/recipes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  recipes = [];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
