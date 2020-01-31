2cef156a59efd34e83407ab70906ce87db7ce4b3
seungyeop-lee <xbeast7@gmail.com>
2020-01-31 18:54:21 +0900

    16. Server Setup Boilerplate Code
    Section 4: [Hands-On] gRPC Project Overview & Setup

---
+++ greet/greet_server/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc-hands-on/greet/greetpb"
+	"log"
+	"net"
+)
+
+type server struct{}
+
+func main() {
+	fmt.Println("Hello world")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
