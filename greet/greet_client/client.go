540391df44f9abde3c370a55aee0c0f5493bae15
seungyeop-lee <xbeast7@gmail.com>
2020-01-31 19:00:14 +0900

    17. Client Setup Boilerplate Code
    Section 4: [Hands-On] gRPC Project Overview & Setup

---
+++ greet/greet_client/client.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc-hands-on/greet/greetpb"
+	"log"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client: %f", c)
+}
