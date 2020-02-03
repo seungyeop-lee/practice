dfb6e5c5acc7876edd2c30913e36f34371e7aba6
seungyeop-lee <xbeast7@gmail.com>
2020-02-03 16:51:49 +0900

    54. MongoDB Driver Golang Setup
    Section 10: [Hands-On] CRUD API with MongoDB

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,22 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc"
 	"grpc-hands-on/blog/blogpb"
 	"log"
 	"net"
 	"os"
 	"os/signal"
+	"time"
 )
 
-type server struct {
+var collection *mongo.Collection
 
+type server struct {
+}
+
+type blogItem struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	AuthorID string             `bson:"author_id"`
+	Content  string             `bson:"content"`
+	Title    string             `bson:"title"`
 }
 
 func main() {
 	// if we crash the go code, we get the file name and line number
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	fmt.Println("Connecting to mongoDB")
+
+	// connect to database
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	collection = client.Database("mydb").Collection("blog")
 
 	fmt.Println("Blog Service Started")
 
@@ -46,5 +74,7 @@ func main() {
 	s.Stop()
 	fmt.Println("Closing the listener")
 	lis.Close()
+	fmt.Println("Closing MongoDB Connection")
+	client.Disconnect(ctx)
 	fmt.Println("End of Program")
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,14 @@ module grpc-hands-on
 go 1.13
 
 require (
+	github.com/DataDog/zstd v1.4.4 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.3.3
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.2.1
 	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
 	golang.org/x/sys v0.0.0-20200124204421-9fbb57f87de9 // indirect
 	golang.org/x/text v0.3.2 // indirect
