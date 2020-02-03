d3112eea9f3b74ba1c525794ca2ecd82ea4b23eb
seungyeop-lee <xbeast7@gmail.com>
2020-02-03 16:38:22 +0900

    53. Blog Service Golang Setup
    Section 10: [Hands-On] CRUD API with MongoDB

---
+++ blog/blog_server/server.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc-hands-on/blog/blogpb"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+)
+
+type server struct {
+
+}
+
+func main() {
+	// if we crash the go code, we get the file name and line number
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	fmt.Println("Blog Service Started")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	s := grpc.NewServer(opts...)
+	blogpb.RegisterBlogServiceServer(s, &server{})
+
+	go func() {
+		fmt.Println("Starting Server...")
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	// Wait for Control C to exit
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+
+	// Block until a signal is received
+	<-ch
+	fmt.Println("Stopping the server")
+	s.Stop()
+	fmt.Println("Closing the listener")
+	lis.Close()
+	fmt.Println("End of Program")
+}
