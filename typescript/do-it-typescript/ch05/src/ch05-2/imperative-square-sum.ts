56a09bc8eb3acd8639c03789c2208028dda43b79
seungyeop-lee <xbeast7@gmail.com>
2020-03-21 22:05:06 +0900

    chap5-2 선언형 프로그래밍과 배열
    chap5 배열과 튜플

---
+++ typescript/do-it-typescript/ch05/src/ch05-2/imperative-square-sum.ts
@@ -0,0 +1,4 @@
+let squareSum = 0
+for (let val = 1; val <= 100; ++val)
+    squareSum += val * val
+console.log(squareSum)
---
+++ typescript/do-it-typescript/ch05/src/ch05-2/imperative-sum.ts
@@ -0,0 +1,6 @@
+let sum = 0
+
+for (let val = 1; val <= 100;)
+    sum += val++
+
+console.log(sum)
