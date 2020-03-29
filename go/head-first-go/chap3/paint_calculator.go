7eedd3457264be91ff108da8bbc449009bbdfeb8
seungyeop-lee <xbeast7@gmail.com>
2020-03-29 14:21:55 +0900

    head first go - chap3 함수

---
+++ go/head-first-go/chap3/paint_calculator.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
+
+	area := width * height
+	return area / 10.0, nil
+}
+
+func main() {
+	var amount, total float64
+	amount, err := paintNeeded(4.2, 3.0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%0.2f liters needed\n", amount)
+	total += amount
+
+	amount, err = paintNeeded(5.2, 3.5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%0.2f liters needed\n", amount)
+	total += amount
+
+	fmt.Printf("Total: %0.2f liters\n", total)
+}
