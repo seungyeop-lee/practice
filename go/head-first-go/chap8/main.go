f0b8c343bbb43a38dd6be02f77e5e173888ec495
seungyeop-lee <xbeast7@gmail.com>
2020-03-29 18:52:53 +0900

    head first go - chap9 사용자 정의 타입

---
+++ go/head-first-go/chap8/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Liters float64
+type Milliliters float64
+type Gallons float64
+
+func (l Liters) ToGallons() Gallons {
+	return Gallons(l * 0.264)
+}
+
+func (m Milliliters) ToGallons() Gallons {
+	return Gallons(m * 0.000264)
+}
+
+func (g Gallons) ToLiters() Liters {
+	return Liters(g * 3.785)
+}
+
+func (g Gallons) ToMilliliters() Milliliters {
+	return Milliliters(g * 3785.41)
+}
+
+func main() {
+	soda := Liters(2)
+	fmt.Printf("%0.3f liters equals %0.3f gallons\n", soda, soda.ToGallons())
+	water := Milliliters(500)
+	fmt.Printf("%0.3f milliliters equals %0.3f gallons\n", water, water.ToGallons())
+
+	milk := Gallons(2)
+	fmt.Printf("%0.3f gallons equals %0.3f liters\n", milk, milk.ToLiters())
+	fmt.Printf("%0.3f gallons equals %0.3f milliliters\n", milk, milk.ToMilliliters())
+}
