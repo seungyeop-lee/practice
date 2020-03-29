874c0c706dd8d4c11ef829accb5ab66ea7d3976a
seungyeop-lee <xbeast7@gmail.com>
2020-03-29 18:29:01 +0900

    head first go - chap8 구조체

---
+++ go/head-first-go/chap7/go.mod
@@ -0,0 +1,3 @@
+module github.com/seungyeop-lee/practice/go/head-first-go/chap7
+
+go 1.14
---
+++ go/head-first-go/chap7/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/seungyeop-lee/practice/go/head-first-go/chap7/magazine"
+)
+
+type subscriber struct {
+	name   string
+	rate   float64
+	active bool
+}
+
+func main() {
+	subscriber := magazine.Subscriber{Name: "Aman Singh"}
+	subscriber.Street = "123 Oak St"
+	subscriber.City = "Omaha"
+	subscriber.State = "NE"
+	subscriber.PostalCode = "68111"
+	fmt.Println("Street:", subscriber.Street)
+	fmt.Println("City:", subscriber.City)
+	fmt.Println("State:", subscriber.State)
+	fmt.Println("Postal Code:", subscriber.PostalCode)
+
+	employee := magazine.Employee{Name: "Joy Carr"}
+	employee.Street = "456 Elm St"
+	employee.City = "Portland"
+	employee.State = "OR"
+	employee.PostalCode = "97222"
+	fmt.Println("Street:", employee.Street)
+	fmt.Println("City:", employee.City)
+	fmt.Println("State:", employee.State)
+	fmt.Println("Postal Code:", employee.PostalCode)
+}
