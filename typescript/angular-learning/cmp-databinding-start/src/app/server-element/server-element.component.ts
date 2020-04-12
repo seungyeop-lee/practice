47d49d28c90c4a2950556e6c4e1dda5f537f4a8f
seungyeop-lee <xbeast7@gmail.com>
2020-04-12 12:24:51 +0900

    79. Lifecycle Hooks and Template Access
    Section5: Components & Databinding Deep Dive

--- typescript/angular-learning/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ typescript/angular-learning/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -2,11 +2,11 @@ import {
   AfterContentChecked,
   AfterContentInit, AfterViewChecked, AfterViewInit,
   Component,
-  DoCheck,
+  DoCheck, ElementRef,
   Input,
   OnChanges, OnDestroy,
   OnInit,
-  SimpleChanges,
+  SimpleChanges, ViewChild,
   ViewEncapsulation
 } from '@angular/core';
 
@@ -27,6 +27,7 @@ export class ServerElementComponent implements OnInit,
   // tslint:disable-next-line:no-input-rename
   @Input('srvElement') element: { type: string, name: string, content: string };
   @Input() name: string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
 
   constructor() {
     console.log('constructor called!');
@@ -39,6 +40,7 @@ export class ServerElementComponent implements OnInit,
 
   ngOnInit(): void {
     console.log('ngOnInit called!');
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
   }
 
   ngDoCheck(): void {
@@ -55,6 +57,7 @@ export class ServerElementComponent implements OnInit,
 
   ngAfterViewInit(): void {
     console.log('ngAfterViewInit called!');
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
   }
 
   ngAfterViewChecked(): void {
