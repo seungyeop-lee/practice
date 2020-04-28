1627657d12d0161f09035ac86a63d0424d18e249
seungyeop-lee <xbeast7@gmail.com>
2020-04-28 13:28:09 +0900

    chap3. 패키징과 내장 도구들

---
+++ go/go-in-action/chap3/wordcount/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"wordcount/words"
+)
+
+func main() {
+	filename := os.Args[1]
+
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("파일을 열 때 오류가 발생했습니다.", err)
+		return
+	}
+
+	text := string(contents)
+
+	count := words.CountWords(text)
+
+	fmt.Printf("총 %d 개의 단어를 발견했습니다. \n", count)
+}
