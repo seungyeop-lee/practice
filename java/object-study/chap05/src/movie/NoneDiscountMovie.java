f51ad84868a48399eae0e4dac9d6a85ebf2cc3da
seungyeop-lee <xbeast7@gmail.com>
2020-05-04 19:06:54 +0900

    Chap05-3. 구현을 통한 검증
    Chap05. 책임 할당하기

---
+++ java/object-study/chap05/src/movie/NoneDiscountMovie.java
@@ -0,0 +1,17 @@
+package movie;
+
+import money.Money;
+
+import java.time.Duration;
+
+public class NoneDiscountMovie extends Movie {
+
+    public NoneDiscountMovie(String title, Duration runningTime, Money fee, DiscountCondition... discountConditions) {
+        super(title, runningTime, fee, discountConditions);
+    }
+
+    @Override
+    protected Money calculateDiscountAmount() {
+        return Money.ZERO;
+    }
+}
