e31771cc30ab9d56da58b4725fc360e6ef7fc3a2
seungyeop-lee <xbeast7@gmail.com>
2020-05-11 19:38:14 +0900

    Chap10-3. Phone 다시 살펴보기
    Chap10. 상속과 코드 재사용

--- java/object-study/chap10/src/phone/Phone.java
+++ java/object-study/chap10/src/phone/Phone.java
@@ -2,49 +2,26 @@ package phone;
 
 import money.Money;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Phone {
-    private Money amount;
-    private Duration seconds;
-    private List<Call> calls = new ArrayList<>();
+public abstract class Phone {
     private double taxRate;
+    private List<Call> calls = new ArrayList<>();
 
-    public Phone(Money amount, Duration seconds, double taxRate) {
-        this.amount = amount;
-        this.seconds = seconds;
+    public Phone(double taxRate) {
         this.taxRate = taxRate;
-    }
-
-    public void call(Call call) {
-        calls.add(call);
-    }
-
-    public List<Call> getCalls() {
-        return calls;
-    }
-
-    public Money getAmount() {
-        return amount;
-    }
-
-    public Duration getSeconds() {
-        return seconds;
     }
 
     public Money calculateFee() {
         Money result = Money.ZERO;
 
         for (Call call : calls) {
-            result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            result = result.plus(calculateCallFee(call));
         }
 
-        return result.plus(result.times(taxRate));
+        return result;
     }
 
-    public double getTaxRate() {
-        return taxRate;
-    }
+    abstract protected Money calculateCallFee(Call call);
 }
