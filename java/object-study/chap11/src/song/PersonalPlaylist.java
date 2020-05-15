445a53cb323d2be85dc2caaad3ba8abef73dbd23
seungyeop-lee <xbeast7@gmail.com>
2020-05-15 13:42:26 +0900

    Chap11-1. 상속을 합셩으로 변경하기
    Chap11. 합성과 유연한 설계

---
+++ java/object-study/chap11/src/song/PersonalPlaylist.java
@@ -0,0 +1,14 @@
+package song;
+
+public class PersonalPlaylist {
+    private Playlist playlist = new Playlist();
+
+    public void append(Song song) {
+        playlist.append(song);
+    }
+
+    public void remove(Song song) {
+        playlist.getTracks().remove(song);
+        playlist.getSingers().remove(song.getSinger());
+    }
+}
