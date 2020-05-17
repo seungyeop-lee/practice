3e0da832dc49bcd58599eb12170dd3ca4a0d9be3
seungyeop-lee <xbeast7@gmail.com>
2020-05-17 12:27:53 +0900

    Chap12-4. 동적 메서드 탐색과 다형성
    Chap12. 다형성

---
+++ java/object-study/chap12/src/lecture/FormattedGradeLecture.java
@@ -0,0 +1,13 @@
+package lecture;
+
+import java.util.List;
+
+public class FormattedGradeLecture extends GradeLecture {
+    public FormattedGradeLecture(String title, int pass, List<Grade> grades, List<Integer> scores) {
+        super(title, pass, grades, scores);
+    }
+
+    public String formatAverage() {
+        return String.format("Avg: %1.1f", super.average());
+    }
+}
