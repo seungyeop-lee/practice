ffbb88d1d6982af201bb0fb33609214268ff9ef6
seungyeop-lee <xbeast7@gmail.com>
2020-05-17 12:12:44 +0900

    Chap12-3. 업캐스팅과 동적 바인딩
    Chap12. 다형성

--- java/object-study/chap12/src/lecture/Lecture.java
+++ java/object-study/chap12/src/lecture/Lecture.java
@@ -26,7 +26,7 @@ public class Lecture {
     }
 
     public String evaluate() {
-        return String.format("Pass:%d Fail: %d", passCount(), failCount());
+        return String.format("Pass:%d Fail:%d", passCount(), failCount());
     }
 
     private long passCount() {
--- java/object-study/chap12/src/lecture/Main.java
+++ java/object-study/chap12/src/lecture/Main.java
@@ -19,5 +19,22 @@ public class Main {
                         new Grade("F", 49, 0)),
                 Arrays.asList(81, 95, 75, 40, 45));
         System.out.println(lecture.evaluate());
+
+        Professor professor = new Professor("다익스트라",
+                new Lecture("알고리즘",
+                        70,
+                        Arrays.asList(81, 95, 75, 50, 45)));
+        System.out.println(professor.compileStatistics());
+
+        professor = new Professor("다익스트라",
+                new GradeLecture("객체지향 프로그래밍",
+                        70,
+                        Arrays.asList(new Grade("A", 100, 95),
+                                new Grade("B", 94, 80),
+                                new Grade("C", 79, 70),
+                                new Grade("D", 69, 50),
+                                new Grade("F", 49, 0)),
+                        Arrays.asList(81, 95, 75, 40, 45)));
+        System.out.println(professor.compileStatistics());
     }
 }
