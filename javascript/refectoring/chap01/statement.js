abe499f571bd8c18109da89df108365a94c85ed6
seungyeop-lee <xbeast7@gmail.com>
2020-07-02 23:38:22 +0900

    1.6 계산 단계와 포맷팅 단계 분리하기
    1. 리팩터링: 첫번째 예시

--- javascript/refectoring/chap01/statement.js
+++ javascript/refectoring/chap01/statement.js
@@ -1,79 +1,52 @@
+import fs from "fs";
+import createStatementData from "./createStatementData";
+
 // 공연료 청구서 출력 함수
 function statement(invoice, plays) {
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    for (let perf of invoice.performances) {
-        // 청구 내역을 출력한다.
-        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    }
-
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-    return result;
-
-    function totalAmount() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += amountFor(perf);
-        }
-        return result;
-    }
-
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += volumeCreditsFor(perf);
-        }
-        return result;
-    }
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-            {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber / 100);
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === playFor(aPerformance).type)
-            result += Math.floor(aPerformance.audience / 5);
-        return result
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (playFor(aPerformance).type) {
-            case "tragedy": // 비극
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy": // 희극
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-        }
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
+    return renderPlainText(createStatementData(invoice, plays));
 }
 
-let fs = require('fs');
+function renderPlainText(data) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+    for (let perf of data.performances) {
+        // 청구 내역을 출력한다.
+        result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    return result;
+}
+
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "  <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n";
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US",
+        {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber / 100);
+}
 
 let invoicesRawData = fs.readFileSync('invoices.json');
 let invoices = JSON.parse(invoicesRawData);
 let playsRawData = fs.readFileSync('plays.json');
 let plays = JSON.parse(playsRawData);
 
-const expect = `청구 내역 (고객명: BigCo)
+const plainExpected = `청구 내역 (고객명: BigCo)
   Hamlet: $650.00 (55석)
   As You Like It: $580.00 (35석)
   Othello: $500.00 (40석)
@@ -81,5 +54,19 @@ const expect = `청구 내역 (고객명: BigCo)
 적립 포인트: 47점
 `;
 
-const result = statement(invoices[0], plays);
-console.log(result == expect);
+const plainResult = statement(invoices[0], plays);
+console.log("statement() test result: ", plainResult === plainExpected);
+
+const htmlExpected = `<h1>청구 내역 (고객명: BigCo)</h1>
+<table>
+  <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>
+  <tr><td>Hamlet</td><td>(55석)</td><td>$650.00</td></tr>
+  <tr><td>As You Like It</td><td>(35석)</td><td>$580.00</td></tr>
+  <tr><td>Othello</td><td>(40석)</td><td>$500.00</td></tr>
+</table>
+<p>총액: <em>$1,730.00</em></p>
+<p>적립 포인트: <em>47</em>점</p>
+`;
+
+const htmlResult = htmlStatement(invoices[0], plays);
+console.log("htmlStatement() test result: ", htmlResult === htmlExpected);
