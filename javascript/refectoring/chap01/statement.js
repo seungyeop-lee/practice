54170db746508e16b18b5cb96417f1f061b31a54
seungyeop-lee <xbeast7@gmail.com>
2020-07-02 23:22:23 +0900

    1.4 statement() 함수 쪼개기 - volumeCredits 변수 제거하기
    1. 리팩터링: 첫번째 예시

--- javascript/refectoring/chap01/statement.js
+++ javascript/refectoring/chap01/statement.js
@@ -1,23 +1,34 @@
 // 공연료 청구서 출력 함수
 function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-
         // 청구 내역을 출력한다.
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${usd(totalAmount)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
+
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    }
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
-            {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber/100);
+            {style: "currency", currency: "USD", minimumFractionDigits: 2}).format(aNumber / 100);
     }
 
     function volumeCreditsFor(aPerformance) {
