29398ec955fe7346db8494122824a1d9e40a7d85
seungyeop-lee <xbeast7@gmail.com>
2020-07-02 22:38:44 +0900

    1.3 리팩터링의 첫 단계
    1. 리팩터링: 첫번째 예시

--- javascript/refectoring/chap01/statement.js
+++ javascript/refectoring/chap01/statement.js
@@ -45,9 +45,18 @@ function statement(invoice, plays) {
 
 let fs = require('fs');
 
-let invoicesRawData = fs.readFileSync('invoices.json')
-let invoices = JSON.parse(invoicesRawData)
-let playsRawData = fs.readFileSync('plays.json')
-let plays = JSON.parse(playsRawData)
+let invoicesRawData = fs.readFileSync('invoices.json');
+let invoices = JSON.parse(invoicesRawData);
+let playsRawData = fs.readFileSync('plays.json');
+let plays = JSON.parse(playsRawData);
 
-console.log(statement(invoices[0], plays));
+const expect = `청구 내역 (고객명: BigCo)
+  Hamlet: $650.00 (55석)
+  As You Like It: $580.00 (35석)
+  Othello: $500.00 (40석)
+총액: $1,730.00
+적립 포인트: 47점
+`;
+
+const result = statement(invoices[0], plays);
+console.log(result == expect);
