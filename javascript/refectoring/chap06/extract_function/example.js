8a00f256d72f1d61bf86c3540d05c35428f0cf90
seungyeop-lee <xbeast7@gmail.com>
2020-07-08 22:21:03 +0900

    6.1 함수 추출하기 - 예시: 지역 변수를 사용할 때
    6. 기본적인 리팩터링

--- javascript/refectoring/chap06/extract_function/example.js
+++ javascript/refectoring/chap06/extract_function/example.js
@@ -16,25 +16,28 @@ export function printOwing(invoice) {
         outstanding += o.amount;
     }
 
-    // 마감일(dueDate)을 기록한다.
+    recodeDueDate(invoice);
+
+    printDetails(invoice, outstanding);
+}
+
+function printBanner() {
+    console.log('************************');
+    console.log('******* 고객 채무 ********');
+    console.log('************************');
+}
+
+function recodeDueDate(invoice) {
     const today = Clock.today;
     invoice.dueDate = new Date(
         today.getFullYear(),
         today.getMonth(),
         today.getDate() + 30
     );
+}
 
-    printDetails();
-
-    function printBanner() {
-        console.log('************************');
-        console.log('******* 고객 채무 ********');
-        console.log('************************');
-    }
-
-    function printDetails() {
-        console.log(`고객명: ${invoice.customer}`);
-        console.log(`채무액: ${outstanding}`);
-        console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
-    }
+function printDetails(invoice, outstanding) {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
