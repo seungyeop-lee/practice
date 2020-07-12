4d3b8578f2f0aaf437f9877209cc73d2035fb74c
seungyeop-lee <xbeast7@gmail.com>
2020-07-12 21:48:09 +0900

    6.9 여러 함수를 클래스로 묶기 - 예시 리팩토링 후
    6. 기본적인 리팩터링

--- javascript/refectoring/chap06/combine_functions_into_class/example.js
+++ javascript/refectoring/chap06/combine_functions_into_class/example.js
@@ -14,28 +14,52 @@ function taxThreshold(year) {
     return year;
 }
 
+class Reading {
+    constructor(data) {
+        this._customer = data.customer;
+        this._quantity = data.quantity;
+        this._month = data.month;
+        this._year = data.year;
+    }
+    get customer() {
+        return this._customer;
+    }
+    get quantity() {
+        return this._quantity;
+    }
+    get month() {
+        return this._month;
+    }
+    get year() {
+        return this._year;
+    }
+    get baseCharge() {
+        return baseRate(this.month, this.year) * this.quantity;
+    }
+    get taxableCharge() {
+        return Math.max(0, this.baseCharge - taxThreshold(this.year));
+    }
+}
+
 function client1() {
-    const aReading = acquireReading();
-    const baseCharge =
-        baseRate(aReading.month, aReading.year) * aReading.quantity;
+    const rawReading = acquireReading();
+    const aReading = new Reading(rawReading);
+    const baseCharge = aReading.baseCharge;
     return baseCharge;
 }
 
 function client2() {
-    const aReading = acquireReading();
-    const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
-    const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+    const rawReading = acquireReading();
+    const aReading = new Reading(rawReading);
+    const taxableCharge = aReading.taxableCharge;
     return taxableCharge;
 }
 
 function client3() {
-    const aReading = acquireReading();
-    const basicChargeAmount = calculateBaseCharge(aReading);
+    const rawReading = acquireReading();
+    const aReading = new Reading(rawReading);
+    const basicChargeAmount = aReading.baseCharge;
     return basicChargeAmount;
-
-    function calculateBaseCharge(aReading) {
-        return baseRate(aReading.month, aReading.year) * aReading.quantity;
-    }
 }
 
 describe('', function () {
