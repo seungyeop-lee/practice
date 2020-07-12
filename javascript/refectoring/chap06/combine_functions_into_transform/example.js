c3ae0cc4b98c9bf7f1ce6e2dfa92f687b8b41eab
seungyeop-lee <xbeast7@gmail.com>
2020-07-12 22:02:03 +0900

    6.10 여러 함수를 변환 함수로 묶기 - 예시 코드
    6. 기본적인 리팩터링

---
+++ javascript/refectoring/chap06/combine_functions_into_transform/example.js
@@ -0,0 +1,51 @@
+import { expect } from 'chai';
+
+let reading = { customer: 'ivan', quantity: 10, month: 5, year: 2017 };
+
+function acquireReading() {
+    return reading;
+}
+
+function baseRate(month, year) {
+    return month * year;
+}
+
+function taxThreshold(year) {
+    return year;
+}
+
+function client1() {
+    const aReading = acquireReading();
+    const baseCharge =
+        baseRate(aReading.month, aReading.year) * aReading.quantity;
+    return baseCharge;
+}
+
+function client2() {
+    const aReading = acquireReading();
+    const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+    const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+    return taxableCharge;
+}
+
+function client3() {
+    const aReading = acquireReading();
+    const basicChargeAmount = calculateBaseCharge(aReading);
+    return basicChargeAmount;
+
+    function calculateBaseCharge(aReading) {
+        return baseRate(aReading.month, aReading.year) * aReading.quantity;
+    }
+}
+
+describe('combine_functions_into_transform', function () {
+    it('client1', function () {
+        expect(client1()).equal(100850);
+    });
+    it('client2', function () {
+        expect(client2()).equal(98833);
+    });
+    it('client3', function () {
+        expect(client3()).equal(100850);
+    });
+});
