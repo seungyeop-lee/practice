3a2c3b11180b77bbde8f4c1dff2c8f99b5c91ab8
seungyeop-lee <xbeast7@gmail.com>
2020-07-12 22:42:55 +0900

    6.11 단계 쪼개기 - 예시 리팩토링 후
    6. 기본적인 리팩터링

--- javascript/refectoring/chap06/split_phase/example.js
+++ javascript/refectoring/chap06/split_phase/example.js
@@ -1,18 +1,30 @@
 import { expect } from 'chai';
 
 function priceOrder(product, quantity, shippingMethod) {
+    const priceData = calculatePricingData(product, quantity);
+    return applyShipping(priceData, shippingMethod);
+}
+
+function calculatePricingData(product, quantity) {
     const basePrice = product.basePrice * quantity;
     const discount =
         Math.max(quantity - product.discountThreshold, 0) *
         product.basePrice *
         product.discountRate;
+    return {
+        basePrice: basePrice,
+        quantity: quantity,
+        discount: discount,
+    };
+}
+
+function applyShipping(priceData, shippingMethod) {
     const shippingPerCase =
-        basePrice > shippingMethod.discountThreshold
+        priceData.basePrice > shippingMethod.discountThreshold
             ? shippingMethod.discountFee
             : shippingMethod.feePerCase;
-    const shippingCost = quantity * shippingPerCase;
-    const price = basePrice - discount + shippingCost;
-    return price;
+    const shippingCost = priceData.quantity * shippingPerCase;
+    return priceData.basePrice - priceData.discount + shippingCost;
 }
 
 describe('', function () {
