f7d420722b41080d4b3f7a330078bbf8560681fb
seungyeop-lee <xbeast7@gmail.com>
2020-07-18 16:51:48 +0900

    6.11 단계 쪼개기 - 예시: 명령줄 프로그램 쪼개기(자바) - 리팩토링 후
    6. 기본적인 리팩터링

--- javascript/refectoring/chap06/split_phase/example/src/main/java/example/Example.java
+++ javascript/refectoring/chap06/split_phase/example/src/main/java/example/Example.java
@@ -18,14 +18,29 @@ public class Example {
     }
 
     static long run(String[] args) throws IOException {
+        return countOrders(parseCommandLine(args));
+    }
+
+    private static CommandLine parseCommandLine(String[] args) {
         if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
-        String filename = args[args.length - 1];
-        File input = Paths.get(filename).toFile();
+        CommandLine result = new CommandLine();
+        result.onlyCountReady = Stream.of(args).anyMatch(arg -> "-r".equals(arg));
+        result.filename = args[args.length - 1];
+        return result;
+    }
+
+    private static long countOrders(CommandLine commandLine) throws IOException {
+        File input = Paths.get(commandLine.filename).toFile();
         ObjectMapper mapper = new ObjectMapper();
         Order[] orders = mapper.readValue(input, Order[].class);
-        if (Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
+        if (commandLine.onlyCountReady)
             return Stream.of(orders).filter(o -> "ready".equals(o.status)).count();
         else
             return orders.length;
+    }
+
+    private static class CommandLine {
+        boolean onlyCountReady;
+        String filename;
     }
 }
