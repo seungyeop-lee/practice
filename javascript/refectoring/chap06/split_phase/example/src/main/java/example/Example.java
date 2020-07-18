f2cd60672d656ff9b640c822e68c5b7590d7c7d2
seungyeop-lee <xbeast7@gmail.com>
2020-07-18 17:08:31 +0900

    6.11 단계 쪼개기 - 예시: 첫 번째 단계에 변환기 사용하기(자바) - 예시 코드
    6. 기본적인 리팩터링

--- javascript/refectoring/chap06/split_phase/example/src/main/java/example/Example.java
+++ javascript/refectoring/chap06/split_phase/example/src/main/java/example/Example.java
@@ -18,29 +18,22 @@ public class Example {
     }
 
     static long run(String[] args) throws IOException {
-        return countOrders(parseCommandLine(args));
-    }
-
-    private static CommandLine parseCommandLine(String[] args) {
         if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
-        CommandLine result = new CommandLine();
-        result.onlyCountReady = Stream.of(args).anyMatch(arg -> "-r".equals(arg));
-        result.filename = args[args.length - 1];
-        return result;
+        CommandLine commandLine = new CommandLine();
+        String filename = args[args.length - 1];
+        return countOrders(commandLine, args, filename);
     }
 
-    private static long countOrders(CommandLine commandLine) throws IOException {
-        File input = Paths.get(commandLine.filename).toFile();
+    private static long countOrders(CommandLine commandLine, String[] args, String filename) throws IOException {
+        File input = Paths.get(filename).toFile();
         ObjectMapper mapper = new ObjectMapper();
         Order[] orders = mapper.readValue(input, Order[].class);
-        if (commandLine.onlyCountReady)
+        if (Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
             return Stream.of(orders).filter(o -> "ready".equals(o.status)).count();
         else
             return orders.length;
     }
 
     private static class CommandLine {
-        boolean onlyCountReady;
-        String filename;
     }
 }
