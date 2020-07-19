c7addcac6ac9a447a8fb64265ae93f79f9a81e41
seungyeop-lee <xbeast7@gmail.com>
2020-07-19 23:41:12 +0900

    7.1 레코드 캡슐화하기 - 예시: 중첩된 레코드 캡슐화하기 - 리팩터링 후
    7. 캡슐화

--- javascript/refectoring/chap07/encapsulate_record/example2.js
+++ javascript/refectoring/chap07/encapsulate_record/example2.js
@@ -1,6 +1,23 @@
 import { assert } from 'chai';
+import _ from 'lodash';
 
-const customerData = {
+class CustomerData {
+    constructor(data) {
+        this._data = data;
+    }
+    get rawData() {
+        return _.cloneDeep(this._data);
+    }
+
+    setUsage(customerID, year, month, amount) {
+        this._data[customerID].usages[year][month] = amount;
+    }
+    usage(customerID, year, month) {
+        return this._data[customerID].usages[year][month];
+    }
+}
+
+let customerData = new CustomerData({
     '1920': {
         name: '마틴 파울러',
         id: '1920',
@@ -22,22 +39,29 @@ const customerData = {
         id: '38673',
         // 다른 고객 정보도 같은 형식으로 저장된다.
     },
-};
+});
+
+function getCustomerData() {
+    return customerData;
+}
 
 describe('', function () {
     it('', function () {
         function compareUsage(customerID, laterYear, month) {
-            const later = customerData[customerID].usages[laterYear][month];
-            const earlier =
-                customerData[customerID].usages[laterYear - 1][month];
+            const later = getCustomerData().usage(customerID, laterYear, month);
+            const earlier = getCustomerData().usage(
+                customerID,
+                laterYear - 1,
+                month
+            );
             return { laterAmount: later, charge: later - earlier };
         }
 
         let customerID = '1920';
         let year = '2016';
         let month = '2';
-        customerData[customerID].usages[year][month] = 10;
-        customerData[customerID].usages[year - 1][month] = 20;
+        getCustomerData().setUsage(customerID, year, month, 10);
+        getCustomerData().setUsage(customerID, year - 1, month, 20);
 
         let usage = compareUsage(customerID, year, month);
         assert.deepEqual(usage, { laterAmount: 10, charge: -10 });
