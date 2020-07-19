b1c481dc5dceb6a41cd88798e8ca5377f146d030
seungyeop-lee <xbeast7@gmail.com>
2020-07-19 23:00:00 +0900

    7.1 레코드 캡슐화하기 - 예시: 간단한 레코드 캡슐화하기 - 리팩터링 후
    7. 캡슐화

--- javascript/refectoring/chap07/encapsulate_record/example.js
+++ javascript/refectoring/chap07/encapsulate_record/example.js
@@ -1,20 +1,46 @@
 import { expect } from 'chai';
 
-const organization = { name: '애크미 구스베리', country: 'GB' };
+class Organization {
+    constructor(data) {
+        this._data = data;
+    }
+
+    set name(aString) {
+        this._data.name = aString;
+    }
+    get name() {
+        return this._data.name;
+    }
+    set country(aCountryCode) {
+        this._data.country = aCountryCode;
+    }
+    get country() {
+        return this._data.country;
+    }
+}
+
+const organization = new Organization({
+    name: '애크미 구스베리',
+    country: 'GB',
+});
+
+function getOrganization() {
+    return organization;
+}
 
 describe('', function () {
     it('', function () {
         let result = '';
-        result += `<h1>${organization.name}</h1>`;
-        result += `<h1>${organization.country}</h1>`;
+        result += `<h1>${getOrganization().name}</h1>`;
+        result += `<h1>${getOrganization().country}</h1>`;
 
         let newName = 'new name';
         let newCountry = 'new country';
-        organization.name = newName;
-        organization.country = newCountry;
+        getOrganization().name = newName;
+        getOrganization().country = newCountry;
         let newResult = '';
-        newResult += `<h1>${organization.name}</h1>`;
-        newResult += `<h1>${organization.country}</h1>`;
+        newResult += `<h1>${getOrganization().name}</h1>`;
+        newResult += `<h1>${getOrganization().country}</h1>`;
 
         expect(result).equal('<h1>애크미 구스베리</h1><h1>GB</h1>');
         expect(newResult).equal('<h1>new name</h1><h1>new country</h1>');
