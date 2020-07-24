c86078b95250b9743bbf1b88766120aea320edf2
seungyeop-lee <xbeast7@gmail.com>
2020-07-24 11:00:31 +0900

    7.5 클래스 추출하기 - 예시: 리팩터링 후
    7. 캡슐화

--- javascript/refectoring/chap07/extract_class/example.js
+++ javascript/refectoring/chap07/extract_class/example.js
@@ -1,6 +1,9 @@
 import { assert } from 'chai';
 
 class Person {
+    constructor() {
+        this._telephoneNumber = new TelephoneNumber();
+    }
     get name() {
         return this._name;
     }
@@ -8,18 +11,36 @@ class Person {
         this._name = arg;
     }
     get telephoneNumber() {
-        return `(${this.officeAreaCode}) ${this.officeNumber}`;
+        return this._telephoneNumber.toString();
     }
     get officeAreaCode() {
-        return this._officeAreaCode;
+        return this._telephoneNumber.areaCode;
     }
     set officeAreaCode(arg) {
-        this._officeAreaCode = arg;
+        this._telephoneNumber.areaCode = arg;
     }
     get officeNumber() {
-        return this._officeNumber;
+        return this._telephoneNumber.number;
     }
     set officeNumber(arg) {
+        this._telephoneNumber.number = arg;
+    }
+}
+
+class TelephoneNumber {
+    toString() {
+        return `(${this.areaCode}) ${this.number}`;
+    }
+    get areaCode() {
+        return this._officeAreaCode;
+    }
+    set areaCode(arg) {
+        this._officeAreaCode = arg;
+    }
+    get number() {
+        return this._officeNumber;
+    }
+    set number(arg) {
         this._officeNumber = arg;
     }
 }
