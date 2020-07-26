af1e003a35e4dd552b86bfaf5753f92fe3fc5378
seungyeop-lee <xbeast7@gmail.com>
2020-07-26 23:01:50 +0900

    8.1 함수 옮기기 - 예시: 다른 클래스로 옮기기: 리팩터링 전
    8. 기능 이동

---
+++ javascript/refectoring/chap08/move_function/example2.js
@@ -0,0 +1,62 @@
+import { assert } from 'chai';
+
+class Account {
+    constructor(aAccountType, daysOverdrawn) {
+        this._type = aAccountType;
+        this._daysOverdrawn = daysOverdrawn;
+    }
+
+    // 은행 이자 계산
+    get bankCharge() {
+        let result = 4.5;
+        if (this._daysOverdrawn > 0) {
+            result += this.overdraftCharge;
+        }
+        return result;
+    }
+
+    // 초과 인출 이자 계산
+    get overdraftCharge() {
+        if (this.type.isPremium) {
+            const baseCharge = 10;
+            if (this.daysOverdrawn <= 7) {
+                return baseCharge;
+            } else {
+                return baseCharge + (this.daysOverdrawn - 7) * 0.85;
+            }
+        } else {
+            return this.daysOverdrawn * 1.75;
+        }
+    }
+
+    get daysOverdrawn() {
+        return this._daysOverdrawn;
+    }
+
+    get type() {
+        return this._type;
+    }
+}
+
+class AccountType {
+    constructor(isPremium) {
+        this._isPremium = isPremium;
+    }
+
+    get isPremium() {
+        return this._isPremium;
+    }
+}
+
+describe('', function () {
+    let account1;
+    let account2;
+    beforeEach(function () {
+        account1 = new Account(new AccountType(true), 7);
+        account2 = new Account(new AccountType(false), 8);
+    });
+    it('', function () {
+        assert.equal(account1.bankCharge, 14.5);
+        assert.equal(account2.bankCharge, 18.5);
+    });
+});
