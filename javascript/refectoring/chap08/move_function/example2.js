d7c1471cdd046b0c4f5a67a1b1aaf368216e53b2
seungyeop-lee <xbeast7@gmail.com>
2020-07-26 23:04:57 +0900

    8.1 함수 옮기기 - 예시: 다른 클래스로 옮기기: 리팩터링 후
    8. 기능 이동

--- javascript/refectoring/chap08/move_function/example2.js
+++ javascript/refectoring/chap08/move_function/example2.js
@@ -10,23 +10,9 @@ class Account {
     get bankCharge() {
         let result = 4.5;
         if (this._daysOverdrawn > 0) {
-            result += this.overdraftCharge;
+            result += this.type.overdraftCharge(this.daysOverdrawn);
         }
         return result;
-    }
-
-    // 초과 인출 이자 계산
-    get overdraftCharge() {
-        if (this.type.isPremium) {
-            const baseCharge = 10;
-            if (this.daysOverdrawn <= 7) {
-                return baseCharge;
-            } else {
-                return baseCharge + (this.daysOverdrawn - 7) * 0.85;
-            }
-        } else {
-            return this.daysOverdrawn * 1.75;
-        }
     }
 
     get daysOverdrawn() {
@@ -45,6 +31,20 @@ class AccountType {
 
     get isPremium() {
         return this._isPremium;
+    }
+
+    // 초과 인출 이자 계산
+    overdraftCharge(daysOverdrawn) {
+        if (this.isPremium) {
+            const baseCharge = 10;
+            if (daysOverdrawn <= 7) {
+                return baseCharge;
+            } else {
+                return baseCharge + (daysOverdrawn - 7) * 0.85;
+            }
+        } else {
+            return daysOverdrawn * 1.75;
+        }
     }
 }
 
