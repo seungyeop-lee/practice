61f7877f86a5ce8f528149a24dcf30c86f515de4
seungyeop-lee <xbeast7@gmail.com>
2020-07-27 08:47:07 +0900

    8.4 문장을 호출한 곳으로 옮기기 - 예시: 리팩터링 전
    8. 기능 이동

---
+++ javascript/refectoring/chap08/move_statements_to_callers/example.js
@@ -0,0 +1,88 @@
+import { assert } from 'chai';
+
+function renderPerson(outStream, person) {
+    outStream.write(`<p>${person.name}</p>\n`);
+    renderPhoto(outStream, person.photo);
+    emitPhotoData(outStream, person.photo);
+
+    function renderPhoto(aPhoto) {
+        return `<p>${aPhoto.binary}</p>`;
+    }
+}
+
+function listRecentPhotos(outStream, photos) {
+    photos
+        .filter((p) => p.date > recentDateCutoff())
+        .forEach((p) => {
+            outStream.write(`<div>\n`);
+            emitPhotoData(outStream, p);
+            outStream.write(`</div>\n`);
+        });
+
+    function recentDateCutoff() {
+        return new Date(2019, 7, 27, 0, 0, 0);
+    }
+}
+
+function emitPhotoData(outStream, photo) {
+    outStream.write(`<p>제목: ${photo.title}</p>\n`);
+    outStream.write(`<p>날짜: ${photo.date.toDateString()}</p>\n`);
+    outStream.write(`<p>위치: ${photo.location}</p>\n`);
+}
+
+class OutStream {
+    constructor(initString) {
+        this._string = initString;
+    }
+
+    write(aString) {
+        this._string += aString;
+    }
+
+    toString() {
+        return this._string;
+    }
+}
+
+describe('move statements to callers1', function () {
+    let photo;
+    let person;
+    beforeEach(function () {
+        photo = {
+            binary: 'test binary',
+            title: 'test title',
+            location: 'test location',
+            date: new Date(2020, 7, 27, 0, 0, 0),
+        };
+        person = {
+            name: 'lee',
+            photo: photo,
+        };
+    });
+    it('renderPerson', function () {
+        let outStream = new OutStream('');
+        renderPerson(outStream, person);
+        assert.equal(
+            outStream.toString(),
+            `<p>lee</p>
+<p>제목: test title</p>
+<p>날짜: Thu Aug 27 2020</p>
+<p>위치: test location</p>
+`
+        );
+    });
+
+    it('listRecentPhotos', function () {
+        let outStream = new OutStream('');
+        listRecentPhotos(outStream, [photo]);
+        assert.equal(
+            outStream.toString(),
+            `<div>
+<p>제목: test title</p>
+<p>날짜: Thu Aug 27 2020</p>
+<p>위치: test location</p>
+</div>
+`
+        );
+    });
+});
