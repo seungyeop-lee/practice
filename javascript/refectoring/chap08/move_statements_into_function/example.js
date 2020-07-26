37c2a3675cbd9e1b03516839709c09c93df7c403
seungyeop-lee <xbeast7@gmail.com>
2020-07-27 00:09:22 +0900

    8.3 문장을 함수로 옮기기 - 예시: 리팩터링 후
    8. 기능 이동

--- javascript/refectoring/chap08/move_statements_into_function/example.js
+++ javascript/refectoring/chap08/move_statements_into_function/example.js
@@ -4,7 +4,6 @@ function renderPerson(outStream, person) {
     const result = [];
     result.push(`<p>${person.name}</p>`);
     result.push(renderPhoto(person.photo));
-    result.push(`<p>제목: ${person.photo.title}</p>`);
     result.push(emitPhotoData(person.photo));
     return result.join('\n');
 }
@@ -14,19 +13,15 @@ function renderPhoto(aPhoto) {
 }
 
 function photoDiv(p) {
-    return [
-        '<div>',
-        `<p>제목: ${p.title}</p>`,
-        emitPhotoData(p),
-        '</div>',
-    ].join('\n');
+    return ['<div>', emitPhotoData(p), '</div>'].join('\n');
 }
 
-function emitPhotoData(aPhoto) {
-    const result = [];
-    result.push(`<p>위치: ${aPhoto.location}</p>`);
-    result.push(`<p>날짜: ${aPhoto.date.toDateString()}</p>`);
-    return result.join('\n');
+function emitPhotoData(p) {
+    return [
+        `<p>제목: ${p.title}</p>`,
+        `<p>위치: ${p.location}</p>`,
+        `<p>날짜: ${p.date.toDateString()}</p>`,
+    ].join('\n');
 }
 
 describe('', function () {
