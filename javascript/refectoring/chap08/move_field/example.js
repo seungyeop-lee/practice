3d4a5bc2b7780215df49b21593e2c733383139d5
seungyeop-lee <xbeast7@gmail.com>
2020-07-26 23:34:02 +0900

    8.2 필드 옮기기 - 예시: 리팩터링 후
    8. 기능 이동

--- javascript/refectoring/chap08/move_field/example.js
+++ javascript/refectoring/chap08/move_field/example.js
@@ -7,26 +7,36 @@ let dateToday = function () {
 class Customer {
     constructor(name, discountRate) {
         this._name = name;
-        this._discountRate = discountRate;
-        this._contract = new CustomerContract(dateToday());
+        this._contract = new CustomerContract(dateToday(), discountRate);
     }
     get discountRate() {
-        return this._discountRate;
+        return this._contract.discountRate;
+    }
+    _setDiscountRate(aNumber) {
+        this._contract.discountRate = aNumber;
     }
 
     becomePreferred() {
-        this._discountRate += 0.03;
+        this._setDiscountRate(this.discountRate + 0.03);
         // 다른 멋진 일들
     }
 
     applyDiscount(amount) {
-        return amount.subtract(amount.multiply(this._discountRate));
+        return amount.subtract(amount.multiply(this.discountRate));
     }
 }
 
 class CustomerContract {
-    constructor(startDate) {
+    constructor(startDate, discountRate) {
         this._startDate = startDate;
+        this._discountRate = discountRate;
+    }
+
+    get discountRate() {
+        return this._discountRate;
+    }
+    set discountRate(arg) {
+        this._discountRate = arg;
     }
 }
 
