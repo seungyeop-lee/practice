dd50f65ad661e843f58d15e099bb30906a33d645
seungyeop-lee <xbeast7@gmail.com>
2020-07-27 23:24:52 +0900

    8.7 반복문 쪼개기 - 예시: 리팩터링 전
    8. 기능 이동

---
+++ javascript/refectoring/chap08/split_loop/example.js
@@ -0,0 +1,37 @@
+import { assert } from 'chai';
+
+function calculate(people) {
+    let youngest = people[0] ? people[0].age : Infinity;
+    let totalSalary = 0;
+    for (const p of people) {
+        if (p.age < youngest) youngest = p.age;
+        totalSalary += p.salary;
+    }
+    return `최연소: ${youngest}, 총 급여: ${totalSalary}`;
+}
+
+describe('calculate', function () {
+    let people;
+    beforeEach(function () {
+        people = [
+            {
+                name: 'lee',
+                age: 34,
+                salary: 200,
+            },
+            {
+                name: 'kim',
+                age: 28,
+                salary: 300,
+            },
+            {
+                name: 'park',
+                age: 26,
+                salary: 100,
+            },
+        ];
+    });
+    it('normal', function () {
+        assert.equal(calculate(people), '최연소: 26, 총 급여: 600');
+    });
+});
