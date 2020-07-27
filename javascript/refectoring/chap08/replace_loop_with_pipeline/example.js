1a5f1835142135e9ba9b3388f492ca352b738b8c
seungyeop-lee <xbeast7@gmail.com>
2020-07-27 23:52:24 +0900

    8.8 반복문을 파이프라인으로 바꾸기 - 예시: 리팩터링 후
    8. 기능 이동

--- javascript/refectoring/chap08/replace_loop_with_pipeline/example.js
+++ javascript/refectoring/chap08/replace_loop_with_pipeline/example.js
@@ -2,19 +2,12 @@ import { assert } from 'chai';
 
 function acquireData(input) {
     const lines = input.split('\n');
-    let firstLine = true;
-    const result = [];
-    for (const line of lines) {
-        if (firstLine) {
-            firstLine = false;
-            continue;
-        }
-        if (line.trim() === '') continue;
-        const record = line.split(',');
-        if (record[1].trim() === 'India') {
-            result.push({ city: record[0].trim(), phone: record[2].trim() });
-        }
-    }
+    const result = lines
+        .slice(1)
+        .filter((line) => line.trim() !== '')
+        .map((line) => line.split(','))
+        .filter((record) => record[1].trim() === 'India')
+        .map((record) => ({ city: record[0].trim(), phone: record[2].trim() }));
     return result;
 }
 
