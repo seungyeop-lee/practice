4f790aa600d79b06fcb0cf8844fbe429a9d73982
seungyeop-lee <xbeast7@gmail.com>
2020-07-27 23:26:07 +0900

    8.7 반복문 쪼개기 - 예시: 리팩터링 후
    8. 기능 이동

--- javascript/refectoring/chap08/split_loop/example.js
+++ javascript/refectoring/chap08/split_loop/example.js
@@ -4,8 +4,10 @@ function calculate(people) {
     let youngest = people[0] ? people[0].age : Infinity;
     let totalSalary = 0;
     for (const p of people) {
-        if (p.age < youngest) youngest = p.age;
         totalSalary += p.salary;
+    }
+    for (const p of people) {
+        if (p.age < youngest) youngest = p.age;
     }
     return `최연소: ${youngest}, 총 급여: ${totalSalary}`;
 }
