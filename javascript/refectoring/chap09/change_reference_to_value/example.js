04bc9a7d1d71553074267e4f675d57c9f90ed4cf
seungyeop-lee <xbeast7@gmail.com>
2020-07-29 13:23:24 +0900

    9.4 참조를 값으로 바꾸기 - 예시: 리팩터링 후
    9. 데이터 조직화

--- javascript/refectoring/chap09/change_reference_to_value/example.js
+++ javascript/refectoring/chap09/change_reference_to_value/example.js
@@ -8,28 +8,31 @@ class Person {
         return this._telephoneNumber.areaCode;
     }
     set officeAreaCode(arg) {
-        this._telephoneNumber.areaCode = arg;
+        this._telephoneNumber = new TelephoneNumber(arg, this.officeNumber);
     }
     get officeNumber() {
         return this._telephoneNumber.number;
     }
     set officeNumber(arg) {
-        this._telephoneNumber.number = arg;
+        this._telephoneNumber = new TelephoneNumber(this.officeAreaCode, arg);
     }
 }
 
 class TelephoneNumber {
+    constructor(areaCode, number) {
+        this._areaCode = areaCode;
+        this._number = number;
+    }
     get areaCode() {
         return this._areaCode;
-    }
-    set areaCode(arg) {
-        this._areaCode = arg;
     }
     get number() {
         return this._number;
     }
-    set number(arg) {
-        this._number = arg;
+
+    equals(other) {
+        if (!(other instanceof TelephoneNumber)) return false;
+        return this.areaCode === other.areaCode && this.number === other.number;
     }
 }
 
@@ -43,5 +46,12 @@ describe('Person', function () {
         person.officeNumber = '1234-5678';
         assert.equal(person.officeAreaCode, '02');
         assert.equal(person.officeNumber, '1234-5678');
+    });
+    it('telephone equals', function () {
+        assert.isTrue(
+            new TelephoneNumber('02', '1234-5678').equals(
+                new TelephoneNumber('02', '1234-5678')
+            )
+        );
     });
 });
