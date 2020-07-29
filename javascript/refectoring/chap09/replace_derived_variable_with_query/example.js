9ca7a9594c8ee8d58926e3ce457fb9c7c7c1894e
seungyeop-lee <xbeast7@gmail.com>
2020-07-29 13:00:21 +0900

    9.3 파생 변수를 질의 함수로 바꾸기 - 예시: 리팩터링 후
    9. 데이터 조직화

--- javascript/refectoring/chap09/replace_derived_variable_with_query/example.js
+++ javascript/refectoring/chap09/replace_derived_variable_with_query/example.js
@@ -7,12 +7,11 @@ class ProductionPlan {
     }
 
     get production() {
-        return this._production;
+        return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
     }
 
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
-        this._production += anAdjustment.amount;
     }
 }
 
