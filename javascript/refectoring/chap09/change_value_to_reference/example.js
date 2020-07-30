4c7603e8db20b2e55b780b3a80fb8a2e9bdecdf4
seungyeop-lee <xbeast7@gmail.com>
2020-07-30 20:40:58 +0900

    9.5 값을 참조로 바꾸기 - 예시: 리팩터링 후
    9. 데이터 조직화

--- javascript/refectoring/chap09/change_value_to_reference/example.js
+++ javascript/refectoring/chap09/change_value_to_reference/example.js
@@ -1,9 +1,24 @@
 import { assert } from 'chai';
 
+let _repositoryData;
+
+export function initialize() {
+    _repositoryData = {};
+    _repositoryData.customers = new Map();
+}
+export function registerCustomer(id) {
+    if (!_repositoryData.customers.has(id))
+        _repositoryData.customers.set(id, new Customer(id));
+    return findCustomer(id);
+}
+export function findCustomer(id) {
+    return _repositoryData.customers.get(id);
+}
+
 class Order {
     constructor(data) {
         this._number = data.number;
-        this._customer = new Customer(data.customer);
+        this._customer = registerCustomer(data.customer);
         // 다른 데이터를 읽어 들인다.
     }
 
@@ -25,13 +40,18 @@ class Customer {
 describe('', function () {
     let orders = [];
     beforeEach(function () {
+        initialize();
         orders.push(new Order({ customer: 'first', number: 10 }));
         orders.push(new Order({ customer: 'second', number: 20 }));
         orders.push(new Order({ customer: 'third', number: 30 }));
+        orders.push(new Order({ customer: 'second', number: 40 }));
+        orders.push(new Order({ customer: 'third', number: 50 }));
     });
     it('', function () {
         assert.equal(orders[0].customer.id, 'first');
         assert.equal(orders[1].customer.id, 'second');
         assert.equal(orders[2].customer.id, 'third');
+        assert.equal(orders[3].customer.id, 'second');
+        assert.equal(orders[4].customer.id, 'third');
     });
 });
