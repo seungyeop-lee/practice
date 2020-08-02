0e25a7d197cec5463cd50af965a56b65f0702abf
seungyeop-lee <xbeast7@gmail.com>
2020-08-02 22:14:05 +0900

    10.2 조건식 통합하기 - 예시: or 사용하기: 리팩터링 후
    10. 조건부 로직 간소화

--- javascript/refectoring/chap10/consolidate_conditional_expression/example.js
+++ javascript/refectoring/chap10/consolidate_conditional_expression/example.js
@@ -1,10 +1,16 @@
 import { assert } from 'chai';
 
 function disabilityAmount(anEmployee) {
-    if (anEmployee.seniority < 2) return 0;
-    if (anEmployee.monthsDisabled > 12) return 0;
-    if (anEmployee.isPartTime) return 0;
+    if (isNotEligibleForDisability()) return 0;
     return 100;
+
+    function isNotEligibleForDisability() {
+        return (
+            anEmployee.seniority < 2 ||
+            anEmployee.monthsDisabled > 12 ||
+            anEmployee.isPartTime
+        );
+    }
 }
 
 describe('disabilityAmount', function () {
