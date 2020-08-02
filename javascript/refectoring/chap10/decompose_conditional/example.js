a48ea8654f070323a09c184b3ae54ff4e1496d3e
seungyeop-lee <xbeast7@gmail.com>
2020-08-02 21:45:35 +0900

    10.1 조건문 분해하기 - 예시: 리팩터링 후
    10. 조건부 로직 간소화

--- javascript/refectoring/chap10/decompose_conditional/example.js
+++ javascript/refectoring/chap10/decompose_conditional/example.js
@@ -15,36 +15,42 @@ class CustomDate {
 }
 
 function calculate(aDate, quantity, plan) {
-    let charge;
-    if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
-        charge = quantity * plan.summerRate;
-    else charge = quantity * plan.regularRate + plan.regularServiceCharge;
-    return charge;
+    return summer() ? summerCharge() : regularCharge();
+
+    function summer() {
+        return (
+            !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd)
+        );
+    }
+
+    function summerCharge() {
+        return quantity * plan.summerRate;
+    }
+
+    function regularCharge() {
+        return quantity * plan.regularRate + plan.regularServiceCharge;
+    }
 }
 
 describe('calculate', function () {
-    it('summer', function () {
-        let date = new CustomDate(new Date(2020, 8, 2));
-        let quantity = 10000;
-        let plan = {
+    let quantity;
+    let plan;
+    beforeEach(function () {
+        quantity = 10000;
+        plan = {
             summerStart: new Date(2020, 6, 17),
             summerEnd: new Date(2020, 8, 30),
             summerRate: 1.3,
             regularRate: 1.1,
             regularServiceCharge: 1000,
         };
+    });
+    it('summer', function () {
+        let date = new CustomDate(new Date(2020, 8, 2));
         assert.equal(calculate(date, quantity, plan), 13000);
     });
     it('not summer', function () {
         let date = new CustomDate(new Date(2020, 10, 2));
-        let quantity = 10000;
-        let plan = {
-            summerStart: new Date(2020, 6, 17),
-            summerEnd: new Date(2020, 8, 30),
-            summerRate: 1.3,
-            regularRate: 1.1,
-            regularServiceCharge: 1000,
-        };
         assert.equal(calculate(date, quantity, plan), 12000);
     });
 });
