80d16b334bfcbdc8994215e63ab72a0f174567ff
seungyeop-lee <xbeast7@gmail.com>
2020-08-02 22:17:10 +0900

    10.2 조건식 통합하기 - 예시: and 사용하기: 리팩터링 전
    10. 조건부 로직 간소화

---
+++ javascript/refectoring/chap10/consolidate_conditional_expression/example2.js
@@ -0,0 +1,27 @@
+import { assert } from 'chai';
+
+function calculate(anEmployee) {
+    if (anEmployee.onVacation) if (anEmployee.seniority > 10) return 1;
+    return 0.5;
+}
+
+describe('', function () {
+    let employee;
+    beforeEach(function () {
+        employee = {
+            onVacation: true,
+            seniority: 11,
+        };
+    });
+    it('return 1', function () {
+        assert.equal(calculate(employee), 1);
+    });
+    it('not vacation', function () {
+        employee.onVacation = false;
+        assert.equal(calculate(employee), 0.5);
+    });
+    it('seniority is low', function () {
+        employee.seniority = 10;
+        assert.equal(calculate(employee), 0.5);
+    });
+});
