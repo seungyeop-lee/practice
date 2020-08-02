96b17b7ec2449e41681238c4a0a45f2a4c70e9b3
seungyeop-lee <xbeast7@gmail.com>
2020-08-02 22:47:22 +0900

    10.3 중첩 조건문을 보호 구문으로 바꾸기 - 예시: 조건 반대로 만들기: 리팩터링 후
    10. 조건부 로직 간소화

--- javascript/refectoring/chap10/replace_nested_conditional_with_guard_clauses/example2.js
+++ javascript/refectoring/chap10/replace_nested_conditional_with_guard_clauses/example2.js
@@ -1,15 +1,17 @@
 import { assert } from 'chai';
 
 function adjustedCapital(anInstrument) {
-    let result = 0;
-    if (anInstrument.capital > 0) {
-        if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-            result =
-                (anInstrument.income / anInstrument.duration) *
-                anInstrument.adjustmentFactor;
-        }
+    if (
+        anInstrument.capital <= 0 ||
+        anInstrument.interestRate <= 0 ||
+        anInstrument.duration <= 0
+    ) {
+        return 0;
     }
-    return result;
+    return (
+        (anInstrument.income / anInstrument.duration) *
+        anInstrument.adjustmentFactor
+    );
 }
 
 describe('adjustedCapital', function () {
