cc174ee154e388fe4d23c52188a5cbd0fbbdfd6d
seungyeop-lee <xbeast7@gmail.com>
2020-08-31 15:21:37 +0900

    02-1. 배열 - 다차원 배열
    02. 기본 자료구조

---
+++ java/do-it-algorithms/src/chap02/section1/paragraph8/exercise/Int2DArray.java
@@ -0,0 +1,19 @@
+package chap02.section1.paragraph8.exercise;
+
+
+//2행 4열의 2차원 배열
+public class Int2DArray {
+    public static void main(String[] args) {
+        int[][] x = new int[2][4];
+
+        x[0][1] = 37;
+        x[0][3] = 54;
+        x[1][2] = 65;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.println("x[" + i + "][" + j + "] = " + x[i][j]);
+            }
+        }
+    }
+}
